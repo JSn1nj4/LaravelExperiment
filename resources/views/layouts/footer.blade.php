de7ae73b90dac74d935c741b0336c6f725a21046
Elliot Derhay <e.j.derhay@gmail.com>
2018-06-10 22:17:05 -0400

    Add basic footer content

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="bg-black w-full absolute pin-b">
-    <div class="p-6">
-
+    <div class="p-6 text-center text-white">
+        <p>Copyright &copy; {{today()->year}} Elliot Derhay</p>
     </div>
 </footer>
