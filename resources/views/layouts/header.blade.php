1a164bc6d1c0fbb4adb78c4fe4cf03196aa89e8a
Elliot Derhay <e.j.derhay@gmail.com>
2018-06-11 21:38:31 -0400

    Play with some font, padding and margin utilities

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,26 +1,24 @@
 <header class="mb-4">
-    <nav class="flex items-center justify-between flex-wrap bg-black p-6">
-        <div class="flex items-center flex-no-shrink text-white mr-6">
-            <a href="/" class="text-white no-underline hover:text-sea-green">
-                <span class="font-semibold text-xl tracking-tight">
+    <nav class="flex items-center justify-between flex-wrap bg-black">
+        <div class="flex items-center flex-no-shrink text-white">
+            <a href="/" class="text-white no-underline hover:text-sea-green px-5 py-5">
+                <span class="text-3xl tracking-tight py-px2">
                     My Project
                 </span>
             </a>
         </div>
-        <div class="block lg:hidden">
+        <div class="block lg:hidden mr-5">
             <button class="flex items-center px-3 py-2 border rounded text-sea-green border-sea-green hover:text-white hover:border-white">
                 <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
             </button>
         </div>
-        <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto text-center lg:text-right">
-            <div class="text-sm lg:flex-grow">
-                <a href="/" class="block mt-4 lg:inline-block lg:mt-0 text-sea-green hover:text-white lg:mr-4">
+        <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto text-center lg:text-right text-xl">
+            <div class="text-md lg:flex-grow">
+                <a href="/" class="block lg:inline-block px-4 py-6 text-sea-green hover:text-white no-underline uppercase">
                     Home
-                </a>
-                <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-sea-green hover:text-white lg:mr-4">
+                </a><a href="#" class="block lg:inline-block px-4 py-6 text-sea-green hover:text-white no-underline uppercase">
                     Projects
-                </a>
-                <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-sea-green hover:text-white">
+                </a><a href="#" class="block lg:inline-block px-4 py-6 text-sea-green hover:text-white no-underline uppercase">
                     Updates
                 </a>
             </div>
