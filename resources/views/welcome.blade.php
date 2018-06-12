cbd7c82a48e5e5eda9572af3ad058293277e2187
Elliot Derhay <e.j.derhay@gmail.com>
2018-06-12 14:17:51 -0400

    Remove sans font from body text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,27 +1,27 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1 class="font-sans font-light mb-4 text-sea-green-dark">
+    <h1 class="font-light mb-4 text-sea-green-dark">
         Test title
     </h1>
 
-    <p class="font-sans mb-4">
+    <p class="mb-4">
         Lorem ipsum dolor sit amet, ei quo velit dicunt indoctum. Usu malis epicurei in, qui ne alia tempor rationibus. Minim nostro an sed, te vim audire blandit, no ornatus vulputate has. Zril utinam suscipiantur ut per, vim no persius omnesque officiis, ad semper impetus est. Ut mel dolor tollit delicata. Ne mei doctus dolorum, ex per paulo recusabo definiebas.
     </p>
 
-    <p class="font-sans mb-4">
+    <p class="mb-4">
         Pro at munere eirmod facilisis, brute ubique intellegebat cu sit. In integre percipit periculis qui. Vim in alia nusquam, per in soluta sanctus corrumpit, wisi paulo delicatissimi id vim. Cum ex phaedrum explicari principes, sea zril volumus eu, ut praesent laboramus urbanitas has. Vix nonumy commodo periculis te, ex ius clita putent consequuntur, dicat paulo forensibus sit et. Ei mel graeci oblique fierent, platonem inciderint ad sit, mundi noster admodum has at.
     </p>
 
-    <p class="font-sans mb-4">
+    <p class="mb-4">
         Antiopam consequat intellegam te mea, ea omittam concludaturque quo. Graeco apeirian ut usu. Eos ea affert gubergren adversarium. Pri at alii dicunt nostrud.
     </p>
 
-    <p class="font-sans mb-4">
+    <p class="mb-4">
         At nobis ignota suscipit usu, eum at iudicabit accusamus, quo ludus dignissim delicatissimi ex. Populo facilis civibus no duo. Omnesque legendos cum te, duo ne tale unum necessitatibus. Et debitis ponderum cum.
     </p>
 
-    <p class="font-sans mb-4">
+    <p class="mb-4">
         An vix quaeque fierent nominati. Eum ne postea accusam, ei partem definiebas pro. Ius tacimates sapientem an. Ne quot indoctum vel. Habemus appareat deterruisset eu duo, ea eum duis tritani suscipiantur.
     </p>
 @endsection
