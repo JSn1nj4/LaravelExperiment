f09dcfac80d0251ff399de6de42bd19d207dc359
JSn1nj4 <e.j.derhay@gmail.com>
2018-06-17 08:27:38 -0400

    Make indentation 4 spaces

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -14,11 +14,11 @@
         </div>
         <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto text-center lg:text-right text-xl">
             <div class="text-md lg:flex-grow">
-              @foreach (['home','projects','updates'] as $key => $value)
-                <a href="{{ route($value) }}" class="block lg:inline-block px-4 py-6 text-sea-green hover:text-white trans-color no-underline uppercase">
-                    {{ $value }}
-                </a>
-              @endforeach
+                @foreach (['home','projects','updates'] as $key => $value)
+                    <a href="{{ route($value) }}" class="block lg:inline-block px-4 py-6 text-sea-green hover:text-white trans-color no-underline uppercase">
+                        {{ $value }}
+                    </a>
+                @endforeach
             </div>
         </div>
     </nav>
