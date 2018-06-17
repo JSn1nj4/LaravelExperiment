b5dc1f9e33a7a207b818306b2de07430ae66618f
JSn1nj4 <e.j.derhay@gmail.com>
2018-06-17 08:34:16 -0400

    Move more code towards 4-space indentation
    It does appear a little less noisy, so hopefully this will be helpful.

--- resources/views/coming-soon.blade.php
+++ resources/views/coming-soon.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.master')
 
 @section('content')
-  <h1 class="font-thin mb-4 text-sea-green-dark text-center">
-    Coming soon!
-  </h1>
+    <h1 class="font-thin mb-4 text-sea-green-dark text-center">
+        Coming soon!
+    </h1>
 
-  <p class="mb-4 text-center">
-    Check back later for some cool stuff!
-  </p>
+    <p class="mb-4 text-center">
+        Check back later for some cool stuff!
+    </p>
 @endsection
