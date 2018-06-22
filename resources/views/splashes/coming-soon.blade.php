6ca0fb1f4a1705bb574462df8fb5908643b847b9
JSn1nj4 <e.j.derhay@gmail.com>
2018-06-21 22:31:47 -0400

    Start putting together a real coming soon splash page

--- resources/views/splashes/coming-soon.blade.php
+++ resources/views/splashes/coming-soon.blade.php
@@ -1,11 +1,18 @@
 @extends('layouts.splash')
 
 @section('content')
-    <h1 class="font-thin mb-4 text-sea-green-dark text-center">
-        Coming soon!
-    </h1>
+    <div class="max-w-lg mx-auto">
+        
+        <div style="display: inline-block; height: 100%; verbatim">
 
-    <p class="mb-4 text-center">
-        Check back later for some cool stuff!
-    </p>
+            <h1 class="mb-4 text-center text-5xl font-normal">
+                ElliotDerhay(<span class="text-sea-green text-4xl leading-none py-3" style="vertical-align: middle;">/{{ Request::path() }}</span>)
+            </h1>
+
+            <p class="mb-4 text-center text-2xl">
+                This site is still in the works. Thanks for your interest! We'll have something ready soon.
+            </p>
+
+        </div>
+    </div>
 @endsection
