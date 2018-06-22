65969425d524dd25acb702ea9d31ff23e71020ca
JSn1nj4 <e.j.derhay@gmail.com>
2018-06-21 21:00:39 -0400

    Create new "maintenance" splash partial

---
+++ resources/views/splashes/maintenance.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.page')
+
+@section('content')
+    <h1 class="font-thin mb-4 text-sea-green-dark text-center">
+        Maintenance Mode
+    </h1>
+
+    <p class="mb-4 text-center">
+        This site is under repair. Please check back later.
+    </p>
+@endsection
