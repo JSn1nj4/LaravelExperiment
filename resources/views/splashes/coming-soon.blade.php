ed0069892078afadddbe27d348a51ddd7b2e9bae
Elliot Derhay <e.j.derhay@gmail.com>
2018-06-29 13:23:16 -0400

    Update Coming Soon splash page again

--- resources/views/splashes/coming-soon.blade.php
+++ resources/views/splashes/coming-soon.blade.php
@@ -4,12 +4,12 @@
     <div class="max-w-lg mx-auto">
         <div style="display: flex; flex-direction: column; justify-content: center; max-height: 100vh; height: 500px;">
 
-            <h1 class="mb-4 text-center text-5xl text-sea-green font-normal">
+            <h1 class="mb-4 text-center text-5xl font-normal">
                 Coming Soon
             </h1>
 
             <p class="mb-4 text-center text-2xl">
-                This site is still in the works. Thanks for your interest! We'll have something ready soon.
+                This site is in the works.
             </p>
 
         </div>
