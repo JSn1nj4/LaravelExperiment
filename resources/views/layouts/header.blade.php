479e1d2bf75bca30e5e2206c352867340f2dc63f
JSn1nj4 <e.j.derhay@gmail.com>
2018-07-08 10:56:29 -0400

    Begin making header nav adjustable
    This will allow switching between having a menu and not having one.

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,25 +1,35 @@
 <header class="mb-4">
     <nav class="flex items-center justify-between flex-wrap bg-black">
-        <div class="flex items-center flex-no-shrink text-white">
-            <a href="/" class="text-white no-underline px-5 py-5">
-                <span class="text-3xl tracking-tight py-px2">
-                    My Project
-                </span>
-            </a>
-        </div>
-        <div class="block lg:hidden mr-5">
-            <button class="flex items-center px-3 py-2 border rounded text-sea-green border-sea-green hover:text-white hover:border-white">
-                <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-            </button>
-        </div>
-        <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto text-center lg:text-right text-xl">
-            <div class="text-md lg:flex-grow">
-                @foreach (['home','projects','updates'] as $key => $value)
-                    <a href="{{ route($value) }}" class="block lg:inline-block px-4 py-6 text-sea-green hover:text-white trans-color no-underline uppercase">
-                        {{ $value }}
-                    </a>
-                @endforeach
+
+        @if (count($menuItems = ['home','projects','updates']) > 0)
+
+            <div class="flex items-center flex-no-shrink text-white">
+                <a href="/" class="text-white no-underline px-5 py-5">
+                    <span class="text-3xl tracking-tight py-px2">
+                        My Project
+                    </span>
+                </a>
             </div>
-        </div>
+
+            <div class="block lg:hidden mr-5">
+                <button class="flex items-center px-3 py-2 border rounded text-sea-green border-sea-green hover:text-white hover:border-white">
+                    <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                </button>
+            </div>
+
+            <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto text-center lg:text-right text-xl">
+                <div class="text-md lg:flex-grow">
+
+                    @foreach ($menuItems as $key => $value)
+                        <a href="{{ route($value) }}" class="block lg:inline-block px-4 py-6 text-sea-green hover:text-white trans-color no-underline uppercase">
+                            {{ $value }}
+                        </a>
+                    @endforeach
+
+                </div>
+            </div>
+
+        @endif
+
     </nav>
 </header>
