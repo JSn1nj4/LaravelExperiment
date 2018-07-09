fccbf8569366658e0019368198e1621b226d4813
JSn1nj4 <e.j.derhay@gmail.com>
2018-07-08 20:16:22 -0400

    Finally setup satisfactory centered header
    This will be used when no menu items present

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,15 +1,30 @@
-<header class="mb-4">
-    <nav class="flex items-center justify-between flex-wrap bg-black">
+@php
+    $headerClasses          = 'flex items-center justify-between flex-wrap';
+    $logoWrapperClasses     = 'flex items-center flex-no-shrink';
+    $logoClasses            = '';
+    $fullHeader             = true;
 
-        <div class="flex items-center flex-no-shrink text-white">
-            <a href="/" class="text-white no-underline px-5 py-5">
-                <span class="text-3xl tracking-tight py-px2">
-                    My Project
+    if(count($menuItems) < 1) {
+        $headerClasses      = 'text-center';
+        $logoWrapperClasses = '';
+        $logoClasses        = 'inline-block';
+        $fullHeader         = false;
+    }
+@endphp
+
+<header class="mb-4">
+    <nav class="{{ $headerClasses }} bg-black">
+
+        <div class="{{ $logoWrapperClasses }} text-white">
+            <a href="/" class="{{ $logoClasses }} text-white no-underline p-2">
+                <img src="https://www.gravatar.com/avatar/8754c5b823c1f0b00e989447a0345a33" width="60" height="60" alt="ElliotDerhay.com logo" title="Elliot Derhay" class="border-solid border-2 border-white rounded-full align-middle">
+                <span class="text-3xl tracking-tight py-px2 pl-2 align-middle">
+                    Elliot Derhay
                 </span>
             </a>
         </div>
 
-        @if (count($menuItems) > 0)
+        @if ($fullHeader)
 
             <div class="block lg:hidden mr-5">
                 <button class="flex items-center px-3 py-2 border rounded text-sea-green border-sea-green hover:text-white hover:border-white">
