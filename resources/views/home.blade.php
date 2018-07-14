abf7aaef71c99f30c8965f2a8b3e3b9bf97a269d
JSn1nj4 <e.j.derhay@gmail.com>
2018-07-14 19:51:04 -0400

    Redo home content layout again
    Too much space for how little content I expect. Will redo again if necessary.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,16 @@
 
 @section('content')
     <div class="block md:flex">
-        <div class="md:w-1/2 lg:w-2/3 pb-6">
+
+        <div class="block lg:flex lg:w-1/6 pb-8">
+            <div class="px-2">
+
+                <img src="https://s3.amazonaws.com/elliotderhay-com/Elliot.Color2-hd-v2-square.jpg" title="Elliot Derhay" alt="Photo of Elliot Derhay" class="border-white border-4 rounded-full box-glow md:hidden">
+
+            </div>
+        </div>
+
+        <div class="md:w-1/2 lg:w-1/3 pb-6">
             <div class="px-2">
 
                 <h1 class="content-title">
@@ -13,31 +22,24 @@
                     Lorem ipsum dolor sit amet, adhuc cetero aliquid ei sed, usu possim option tincidunt no. Quo an ludus alterum deserunt. Meis paulo pro ex, vitae eligendi cu sit. Alia justo saperet mel cu. Ludus oblique deleniti te vim, iudico nostro incorrupte est in. Ullum quaestio et mel.
                 </p>
 
-                <h2 class="content-title">
-                    Test title h2
-                </h2>
-
-                <p class="mb-4">
-                    Tation salutatus vim id, error consequuntur sea eu, in adhuc offendit singulis eam. Ad pro minimum pertinax neglegentur, ea sit viderer nominati, ne vis malis delenit. Ex habeo melius feugiat per, duis feugiat deserunt sed ei, ne munere noluisse sed. Mel nihil appetere ei.
-                </p>
-
                 <h4 class="content-title">
                     Test small title
                 </h4>
 
                 <p class="mb-4">
-                    Est eu quaeque fabulas ancillae, ex tation praesent hendrerit sit. Est doctus corrumpit ut, stet cetero euripidis ex mel. Ea nam habeo dicit vivendum, sumo dissentiunt eos id, eos no erroribus abhorreant. Melius quaestio qui ei. In per conceptam elaboraret, ei lucilius patrioque disputando ius. Oportere reformidans id vis.
+                    No duo esse signiferumque, at agam iriure cum. Ad ius eirmod fabulas. Ne qui option singulis. Vis porro semper recusabo ei. Utroque graecis corrumpit eu cum, pri quis labore adipiscing no.
                 </p>
 
             </div>
         </div>
 
-        <div class="md:w-1/2 lg:w-1/3 pb-8">
+        <div class="hidden md:flex md:w-1/2 lg:w-1/3 pb-8">
             <div class="px-2">
 
                 <img src="https://s3.amazonaws.com/elliotderhay-com/Elliot.Color2-hd-v2-square.jpg" title="Elliot Derhay" alt="Photo of Elliot Derhay" class="border-white border-4 rounded-full box-glow">
 
             </div>
         </div>
+
     </div>
 @endsection
