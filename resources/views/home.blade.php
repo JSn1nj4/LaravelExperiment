347c88a25c8b3126ad48ee91495b52afd1d3a7b6
JSn1nj4 <e.j.derhay@gmail.com>
2018-07-14 11:58:25 -0400

    Toy with home again, add portrait

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,44 +2,42 @@
 
 @section('content')
     <div class="block md:flex">
-        <div class="md:w-1/2 lg:w-1/3">
-            <h1 class="content-title">
-                Test title
-            </h1>
+        <div class="md:w-1/2 lg:w-2/3">
+            <div class="px-1">
 
-            <h4 class="content-title">
-                Test small title
-            </h4>
+                <h1 class="content-title">
+                    Test title
+                </h1>
 
-            <p class="mb-4">
-                Lorem ipsum dolor sit amet, ei quo velit dicunt indoctum. Usu malis epicurei in, qui ne alia tempor rationibus. Minim nostro an sed, te vim audire blandit, no ornatus vulputate has. Zril utinam suscipiantur ut per, vim no persius omnesque officiis, ad semper impetus est. Ut mel dolor tollit delicata. Ne mei doctus dolorum, ex per paulo recusabo definiebas.
-            </p>
+                <p class="mb-4">
+                    Antiopam consequat intellegam te mea, ea omittam concludaturque quo. Graeco apeirian ut usu. Eos ea affert gubergren adversarium. Pri at alii dicunt nostrud.
+                </p>
+
+                <h2 class="content-title">
+                    Test title h2
+                </h2>
+
+                <p class="mb-4">
+                    At nobis ignota suscipit usu, eum at iudicabit accusamus, quo ludus dignissim delicatissimi ex. Populo facilis civibus no duo. Omnesque legendos cum te, duo ne tale unum necessitatibus. Et debitis ponderum cum.
+                </p>
+
+                <h4 class="content-title">
+                    Test small title
+                </h4>
+
+                <p class="mb-4">
+                    An vix quaeque fierent nominati. Eum ne postea accusam, ei partem definiebas pro. Ius tacimates sapientem an. Ne quot indoctum vel. Habemus appareat deterruisset eu duo, ea eum duis tritani suscipiantur.
+                </p>
+
+            </div>
         </div>
 
-        <div class="md:w-1/2 lg:w-2/3">
-            <h1 class="content-title">
-                Test title
-            </h1>
+        <div class="md:w-1/2 lg:w-1/3">
+            <div class="px-1">
 
-            <p class="mb-4">
-                Antiopam consequat intellegam te mea, ea omittam concludaturque quo. Graeco apeirian ut usu. Eos ea affert gubergren adversarium. Pri at alii dicunt nostrud.
-            </p>
+                <img src="https://s3.amazonaws.com/elliotderhay-com/Elliot.Color2-hd-v2-portrait.jpg" alt="Photo of Elliot Derhay">
 
-            <h2 class="content-title">
-                Test title h2
-            </h2>
-
-            <p class="mb-4">
-                At nobis ignota suscipit usu, eum at iudicabit accusamus, quo ludus dignissim delicatissimi ex. Populo facilis civibus no duo. Omnesque legendos cum te, duo ne tale unum necessitatibus. Et debitis ponderum cum.
-            </p>
-
-            <h4 class="content-title">
-                Test small title
-            </h4>
-
-            <p class="mb-4">
-                An vix quaeque fierent nominati. Eum ne postea accusam, ei partem definiebas pro. Ius tacimates sapientem an. Ne quot indoctum vel. Habemus appareat deterruisset eu duo, ea eum duis tritani suscipiantur.
-            </p>
+            </div>
         </div>
     </div>
 @endsection
