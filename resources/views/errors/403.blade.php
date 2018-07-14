57f0c59cfce651cd7a559181ae7fadedfdcbc8b9
JSn1nj4 <e.j.derhay@gmail.com>
2018-07-13 23:14:17 -0400

    Add first version of 403 page

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.error', [
+    'errorCode' => '403',
+    'errorTitle' => !empty($exception->getMessage()) ? $exception->getMessage() : 'Forbidden'
+])
+
+@section('status-body')
+    <p>
+        Sorry, but you're not authorized to view this resource.
+    </p>
+@endsection
+
+@section('status-footer')
+    <p>
+        <a href="{{ route('home') }}">Back to homepage</a>
+    </p>
+@endsection
