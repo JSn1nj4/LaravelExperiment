4df1a6efe6d1f1b6c7fd28b0097969583954b8b1
JSn1nj4 <e.j.derhay@gmail.com>
2018-07-15 17:27:41 -0400

    Remove GitLab icon from socials
    I primarily use GitLab for private projects right now. I'll show the icon later if it's needed for any other reason.

--- resources/views/partials/socials.blade.php
+++ resources/views/partials/socials.blade.php
@@ -5,6 +5,5 @@
 
 <p class="{{ $classes }}">
     <a href="https://github.com/JSn1nj4" class="{{ $linkClasses }}" target="_blank"><i class="fab fa-github"></i></a>
-    <a href="https://gitlab.com/JSn1nj4" class="{{ $linkClasses }}" target="_blank"><i class="fab fa-gitlab"></i></a>
     <a href="https://twitter.com/JSn1nj4" class="{{ $linkClasses }}" target="_blank"><i class="fab fa-twitter"></i></a>
 </p>
