129529996b22d017523c728f57f313d94448cd0d
JSn1nj4 <e.j.derhay@gmail.com>
2018-07-14 20:48:04 -0400

    Use FontAwesome CDN URL

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.master', ['bodyClasses' => !empty($bodyClasses) ? $bodyClasses : ''])
 
 @section('head-extras')
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+    
     @yield('head-extras')
 @endsection
 
