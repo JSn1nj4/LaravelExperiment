02d0293de195522a25cea95da83ceb17e44fb9b4
JSn1nj4 <e.j.derhay@gmail.com>
2018-07-15 20:28:46 -0400

    Add functional banner section to homepage
    This will work nicely for now. I'll tweak later if necessary.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,63 +2,73 @@
 
 @section('content')
 
-    <div class="container mx-auto px-4 pt-6">
-        <section class="block md:flex banner-section home-banner">
+    <section class="banner-section home-banner -mt-4">
+        <div class="container mx-auto px-4 py-6">
+            <div class="block md:flex banner-content-wrapper">
 
-            <div class="lg:flex lg:w-2/3">
-                <h1>
-                    Elliot Derhay
-                </h1>
-            </div>
 
-            <div class="lg:flex lg:w-1/3">
-
-            </div>
-
-        </section>
-    </div>
-
-    <div class="container mx-auto px-4 pt-6">
-        <section class="block md:flex">
-
-            <div class="block lg:flex lg:w-1/6 pb-8">
-                <div class="px-2">
-
-                    <img src="https://s3.amazonaws.com/elliotderhay-com/Elliot.Color2-hd-v2-square.jpg" title="Elliot Derhay" alt="Photo of Elliot Derhay" class="border-white border-4 rounded-full box-glow md:hidden">
-
-                </div>
-            </div>
-
-            <div class="md:w-1/2 lg:w-1/3 pb-6">
-                <div class="px-2">
-
-                    <h1 class="content-title">
-                        About Me
+                <div class="lg:w-3/5 text-center banner-content">
+                    <h1 class="banner-title pb-4">
+                        ElliotDerhay.com
                     </h1>
-
-                    <p class="mb-4">
-                        I am a web developer primarily with experience in building WordPress websites. I have learned the basics of HTML and CSS along with JavaScript and PHP.
+                    <p class="banner-paragraph leading-tight">
+                        A small website for a personal profile and web development portfolio
                     </p>
+                </div>
 
-                    <p class="mb-4">
-                        Regarding JavaScript frameworks, I have primarily used jQuery, though I did dive into MeteorJS for a while for a personal project -- and it was quite fun to learn and use. I have also learned a bit of React and Vue, though I'm definitely leaning towards Vue.
-                    </p>
-
-                    <p class="mb-4">
-                         For PHP, I have primarily not used a framework (outside of WordPress), though I have been learning Laravel recently; in fact, this website is built using Laravel, and will continue to make use of it for different learning experiments.
-                    </p>
+                <div class="lg:w-2/5">
 
                 </div>
+
             </div>
+        </div>
+    </section>
 
-            <div class="hidden md:flex md:w-1/2 lg:w-1/3 pb-8">
-                <div class="px-2">
+    <section>
+        <div class="container mx-auto px-4 pt-6">
+            <div class="block md:flex">
 
-                    <img src="https://s3.amazonaws.com/elliotderhay-com/Elliot.Color2-hd-v2-square.jpg" title="Elliot Derhay" alt="Photo of Elliot Derhay" class="border-white border-4 rounded-full box-glow">
 
+                <div class="block lg:flex lg:w-1/6 pb-8">
+                    <div class="px-2">
+
+                        <img src="https://s3.amazonaws.com/elliotderhay-com/Elliot.Color2-hd-v2-square.jpg" title="Elliot Derhay" alt="Photo of Elliot Derhay" class="border-white border-4 rounded-full box-glow md:hidden">
+
+                    </div>
                 </div>
-            </div>
 
-        </section>
-    </div>
+                <div class="md:w-1/2 lg:w-1/3 pb-6">
+                    <div class="px-2">
+
+                        <h1 class="content-title">
+                            About Me
+                        </h1>
+
+                        <p class="mb-4">
+                            I am a web developer primarily with experience in building WordPress websites. I have learned the basics of HTML and CSS along with JavaScript and PHP.
+                        </p>
+
+                        <p class="mb-4">
+                            Regarding JavaScript frameworks, I have primarily used jQuery, though I did dive into MeteorJS for a while for a personal project -- and it was quite fun to learn and use. I have also learned a bit of React and Vue, though I'm definitely leaning towards Vue.
+                        </p>
+
+                        <p class="mb-4">
+                             For PHP, I have primarily not used a framework (outside of WordPress), though I have been learning Laravel recently; in fact, this website is built using Laravel, and will continue to make use of it for different learning experiments.
+                        </p>
+
+                    </div>
+                </div>
+
+                <div class="hidden md:flex md:w-1/2 lg:w-1/3 pb-8">
+                    <div class="px-2">
+
+                        <img src="https://s3.amazonaws.com/elliotderhay-com/Elliot.Color2-hd-v2-square.jpg" title="Elliot Derhay" alt="Photo of Elliot Derhay" class="border-white border-4 rounded-full box-glow">
+
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </section>
 @endsection
