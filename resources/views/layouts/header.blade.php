5feb6e3b637b6e18f5996d6cd7e665949a6798b7
Elliot Derhay <e.j.derhay@gmail.com>
2018-07-17 13:11:21 -0400

    Changes to how menu items are generated
    Specifics:
    - Make the 'home' menu item a house icon
    - Add an "active" class when a menu item matches the current route

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -38,8 +38,23 @@
                 <div class="text-md md:flex-grow">
 
                     @foreach ($menuItems as $key => $value)
-                        <a href="{{ route($value) }}" class="{{ $value === 'home' ? "fas fa-home " : ''}}block md:inline-block px-4 py-6 no-underline uppercase"{{ $value === 'home' ? ' title=Home' : ''}}>
-                            {{ $value !== 'home' ? $value : '' }}
+                        @php
+                            $extraClasses = '';
+                            $titleText = '';
+                            $itemText = $value;
+
+                            if($value === 'home') {
+                                $extraClasses = 'fas fa-home ';
+                                $titleText = 'title=Home';
+                                $itemText = '';
+                            }
+                        @endphp
+
+                        <a
+                            href="{{ route($value, [], false) }}"
+                            class="{{ $extraClasses }}block md:inline-block px-4 py-6 no-underline uppercase{{ Route::currentRouteName() === $value ? ' active' : '' }}"
+                            {{ $titleText }}>
+                            {{ $itemText }}
                         </a>
                     @endforeach
 
