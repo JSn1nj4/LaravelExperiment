1dff87364396ed4d9dcdf710fbe19af5a6eec155
Elliot Derhay <e.j.derhay@gmail.com>
2018-07-18 13:19:23 -0400

    Setup sections for more content to be added
    At least some of it will be only text. We'll see where this leads...
    Use only 1 dummy paragraph on homepage
    
    There's no way I'll be able to add more than this right now. I can always update it later.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -32,6 +32,31 @@
 
                 </div>
 
+                <div class="lg:w-2/3 text-justify">
+                    <div class="px-2">
+
+                        <p class="my-4">
+                            Lorem ipsum dolor sit amet, pri in dolor verear maiestatis, per omnis indoctum vituperata ad. Iusto prompta cu per, vero vocent vocibus pro ea. Ex dicat moderatius vis, no ius ludus noluisse, molestie consulatu cu eam. Veri tempor sea in, cu vis possim praesent cotidieque. Ex quas honestatis per. Per fastidii qualisque ne. Vis no diam aeterno definiebas, congue option suscipiantur cu pro.
+                        </p>
+                        <p class="my-4">
+                            Nam dolor lucilius eu, ut per dictas sapientem vulputate, in feugiat evertitur concludaturque pri. Facilis ocurreret nam at. Te duo suscipit intellegat. Saperet minimum vulputate eos cu, error appareat patrioque et mel. Liber nobis honestatis no usu, nam sint prompta ei, ut sed rebum civibus voluptaria. Ei vide aliquando theophrastus eam, accumsan efficiendi nam at. Et sed vivendo persequeris, cu enim nulla oportere per, ad sea praesent dignissim dissentias.
+                        </p>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container mx-auto px-4 pt-6">
+            <div class="block md:flex">
+
+                <div class="hidden lg:flex lg:w-1/6">
+
+                </div>
+
                 <div class="md:w-1/2 lg:w-1/3 pb-6 text-center">
                     <div class="px-2">
 
