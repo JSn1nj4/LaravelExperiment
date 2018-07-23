2b570b5bf6b8235d0667e591d1559a5f36025303
Elliot Derhay <e.j.derhay@gmail.com>
2018-07-22 21:19:01 -0400

    Rewrite MaintenanceMode similarly to the ComingSoon class
    The main difference is that Maintenance Mode will not override the Coming Soon setting.

--- app/Http/Middleware/MaintenanceMode.php
+++ app/Http/Middleware/MaintenanceMode.php
@@ -15,14 +15,53 @@ class MaintenanceMode
      */
     public function handle($request, Closure $next)
     {
-        if(config('app.maintenance') && !$request->is('maintenance') && !$request->is('coming-soon')) {
+        if($this->shouldRedirect($request)) {
             return redirect()->route('maintenance');
         }
 
-        if(!config('app.maintenance') && !$request->is('/') && !(config('app.env') === 'local')) {
+        if($this->shouldRedirectHome($request)) {
             return redirect()->route('home');
         }
 
         return $next($request);
+    }
+
+    /**
+     * Check if the user should be redirected
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     *
+     * This method is used to check if the user should be redirected to the
+     * Coming Soon page. This method is used in place of inlining the below
+     * expression within an if statement.
+     */
+    private function shouldRedirect($request)
+    {
+        return config('app.maintenance') && !$request->is('maintenance') && !$request->is('coming-soon');
+    }
+
+    /**
+     * Check if the user should be redirected home
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     *
+     * This method is used to check if the user should be redirected to the
+     * homepage instead. Similar to the previous method, this method is used
+     * in place of inlining the below logic in an `if` statement.
+     *
+     * Also, the reason for the last part of the expression is that I want to be
+     * able to manually open the Coming Soon page in case I need to work on it.
+     *
+     * Lastly, this method will prevent redirecting to the homepage if the
+     * Coming Soon setting is on.
+     */
+    private function shouldRedirectHome($request)
+    {
+        return !config('app.maintenance')
+            && !config('app.coming_soon')
+            && !$request->is('/')
+            && !(config('app.env') === 'local');
     }
 }
