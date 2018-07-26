7d2c3c1932626bc9895f8fadc29aed76f509d58d
Elliot Derhay <e.j.derhay@gmail.com>
2018-07-26 12:00:08 -0400

    Finish styling timeline for now
    This includes setting relative positioning for the cards. This was necessary to put them over the top of the border that was used in the timeline, otherwise the border would continue to show on top of the cards.

--- resources/assets/js/components/Timeline.vue
+++ resources/assets/js/components/Timeline.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="timeline">
+  <div class="timeline relative mb-4">
 
+    <div class="absolute pin-t w-10 border-r border-grey-dark border-solid h-full">&nbsp;</div>
     <slot></slot>
+    <div class="absolute pin-b w-10 border-r border-grey-dark border-dashed h-4 -mb-4">&nbsp;</div>
+
   </div>
 </template>
 <script>
