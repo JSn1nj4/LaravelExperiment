55b3a7056d49ca37b03eacc40decbbca41bc2130
Elliot Derhay <e.j.derhay@gmail.com>
2018-07-26 11:21:45 -0400

    Reformat Card component to 2-space indentation

--- resources/assets/js/components/Card.vue
+++ resources/assets/js/components/Card.vue
@@ -1,36 +1,41 @@
 <template>
-    <div :class="classes" @click="click">
-        <slot></slot>
-    </div>
+  <div :class="classes" @click="click">
+    <slot></slot>
+  </div>
 </template>
 <script>
 export default {
-    name: "card",
-    props: {
-        size: String,
-        url: String
-    },
-    data: () => ({
-        urlIsSet: false,
-        cursorClass: ''
-    }),
-    computed: {
-        classes() {
-            return `bg-grey-darkest p-4 my-4 rounded-lg border border-grey-dark trans-border-color hover:border-sea-green overflow-hidden flex max-w-${this.size} w-full${this.cursorClass}`;
-        }
-    },
-    methods: {
-        click() {
-            if(this.urlIsSet) {
-                open(this.url, '_blank');
-            }
-        }
-    },
-    mounted() {
-        if(this.url && this.url.length > 0) {
-            this.cursorClass = ' cursor-pointer';
-            this.urlIsSet = true;
-        }
+  name: "card",
+
+  props: {
+    size: String,
+    url: String
+  },
+
+  data: () => ({
+    urlIsSet: false,
+    cursorClass: ''
+  }),
+
+  computed: {
+    classes() {
+      return `bg-grey-darkest p-4 my-4 rounded-lg border border-grey-dark trans-border-color hover:border-sea-green overflow-hidden flex max-w-${this.size} w-full${this.cursorClass}`;
     }
+  },
+
+  methods: {
+    click() {
+      if(this.urlIsSet) {
+        open(this.url, '_blank');
+      }
+    }
+  },
+
+  mounted() {
+    if(this.url && this.url.length > 0) {
+      this.cursorClass = ' cursor-pointer';
+      this.urlIsSet = true;
+    }
+  }
 }
 </script>
