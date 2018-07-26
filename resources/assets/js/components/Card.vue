50c8bd130af7653cfb9fd881cfa228799200eef2
Elliot Derhay <e.j.derhay@gmail.com>
2018-07-25 23:45:35 -0400

    Put together example Twitter cards
    These will change, but they're heading in the right direction (at least currently).

--- resources/assets/js/components/Card.vue
+++ resources/assets/js/components/Card.vue
@@ -1,6 +1,14 @@
 <template>
-    <div id="">
-
+    <div class="bg-grey-darkest p-4 m-4 rounded-lg border border-white trans-border-color hover:border-sea-green overflow-hidden flex max-w-sm w-full">
+        <div class="flex-none">
+            <a :href="link" target="_blank">
+                <img width="48" height="48" :src="icon" class="border-solid border-2 border-white rounded-full">
+            </a>
+        </div>
+        <div class="pl-4 flex flex-col">
+            <p><a :href="link" target="_blank">{{ title }}</a></p>
+            <p>{{ blurb }}</p>
+        </div>
     </div>
 </template>
 <script>
