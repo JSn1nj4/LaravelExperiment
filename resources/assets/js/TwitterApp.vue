0b3e039aedf65eca67567acc12ac9129bf099444
Elliot Derhay <e.j.derhay@gmail.com>
2018-07-26 20:39:34 -0400

    Finish building out Twitter card template
    Link for reference: https://developer.twitter.com/en/developer-terms/display-requirements.html

--- resources/assets/js/TwitterApp.vue
+++ resources/assets/js/TwitterApp.vue
@@ -4,22 +4,47 @@
     <timeline>
       <card v-for="(tweet, index) in tweets" :size="'sm'" :url="tweet.link" :key="`tweet-${index}`">
 
-        <div class="flex-none">
-          <a :href="tweet.link" target="_blank">
-            <img width="48" height="48" :src="tweet.icon" class="border-solid border-2 border-white rounded-full">
-          </a>
+        <div class="flex flex-row">
+          <div>
+            <a :href="tweet.link" target="_blank">
+              <img width="48" height="48" :src="tweet.icon" class="border-solid border-2 border-white rounded-full">
+            </a>
+          </div>
+
+          <div class="pl-4 flex-grow">
+            <p>
+              <a :href="tweet.link" target="_blank" class="no-underline">
+                {{ tweet.name }}
+              </a><br>
+              <a :href="tweet.link" target="_blank" class="no-underline text-grey-dark">
+                {{ tweet.username }}
+              </a>
+            </p>
+          </div>
+
+          <div class="pl-4 flex-none">
+            <a href="https://twitter.com">
+              <i class="fab fa-twitter text-4xl text-white"></i>
+            </a>
+          </div>
         </div>
 
-        <div class="pl-4 flex flex-col">
-          <p>
-            <a :href="tweet.link" target="_blank" class="no-underline">
-              {{ tweet.name }}
-            </a>
-            <a :href="tweet.link" target="_blank" class="no-underline text-grey-dark">
-              {{ tweet.username }}
-            </a>
-          </p>
-          <p class="pt-3">{{ tweet.blurb }}</p>
+        <div class="pt-4 flex flex-row">
+          <p>{{ tweet.blurb }}</p>
+        </div>
+
+        <div class="pt-4 flex flex-row">
+          <div class="flex-grow">
+            <p class="text-sea-green">
+              <i class="far fa-comment"></i>
+              <i class="fas fa-retweet"></i>
+              <i class="far fa-heart"></i>
+            </p>
+          </div>
+
+          <div class="pl-4 flex-none">
+            <p class="text-sea-green">26 Apr 2018</p>
+          </div>
         </div>
 
       </card>
