9f01d1d8fbd6e502c15ae073dffa99a37b0a479c
Elliot Derhay <e.j.derhay@gmail.com>
2018-07-27 11:55:14 -0400

    First pass at creating TwitterCard
    Error to deal with: `tweet` is not defined. This is the prop named `tweet`.

--- resources/assets/js/TwitterApp.vue
+++ resources/assets/js/TwitterApp.vue
@@ -2,58 +2,13 @@
   <div id="twitter-app" class="max-w-sm m-auto pb-4">
 
     <timeline>
-      <card v-for="(tweet, index) in tweets" :size="'sm'" :url="tweet.link" :key="`tweet-${index}`">
-
-        <div class="flex flex-row relative">
-          <div>
-            <a :href="tweet.user.profile" target="_blank" @click.stop>
-              <img width="48" height="48" :src="tweet.user.avatar" class="border-solid border-2 border-white rounded-full" @click.stop>
-            </a>
-          </div>
-
-          <div class="pl-4 flex-grow relative">
-            <p>
-              <a :href="tweet.user.profile" target="_blank" class="no-underline" @click.stop>
-                {{ tweet.user.name }}
-              </a><br>
-              <a :href="tweet.user.profile" target="_blank" class="no-underline text-grey-dark" @click.stop>
-                {{ tweet.user.handle }}
-              </a>
-            </p>
-          </div>
-
-          <div class="pl-4 flex-none relative">
-            <a :href="baseLink" target="_blank" @click.stop>
-              <i class="fab fa-twitter text-3xl text-white" @click.stop></i>
-            </a>
-          </div>
-        </div>
-
-        <div class="pt-4 flex flex-row relative">
-          <p>{{ tweet.blurb }}</p>
-        </div>
-
-        <div class="pt-4 flex flex-row relative">
-          <div class="flex-grow">
-            <p class="text-sea-green">
-              <i class="far fa-comment hover:text-white trans-color"></i>
-              <i class="fas fa-retweet hover:text-white trans-color"></i>
-              <i class="far fa-heart hover:text-white trans-color"></i>
-            </p>
-          </div>
-
-          <div class="pl-4 flex-none relative">
-            <p class="text-sea-green" @click.stop>26 Apr 2018</p>
-          </div>
-        </div>
-
-      </card>
+      <twitter-card v-for="(tweet, index) in tweets" :tweet="tweet" :key="`tweet-${index}`"></twitter-card>
     </timeline>
 
   </div>
 </template>
 <script>
-import Card from './components/Card.vue';
+import TwitterCard from './components/TwitterCard.vue';
 import Timeline from './components/Timeline.vue';
 
 export default {
@@ -61,7 +16,7 @@ export default {
 
   components: {
     Timeline,
-    Card
+    TwitterCard
   },
 
   data: () => ({
---
+++ resources/assets/js/components/TwitterCard.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="twitter-card">
+
+    <card :size="'sm'" :url="tweet.link">
+
+      <div class="flex flex-row relative">
+        <div>
+          <a :href="tweet.user.profile" target="_blank" @click.stop>
+            <img width="48" height="48" :src="tweet.user.avatar" class="border-solid border-2 border-white rounded-full" @click.stop>
+          </a>
+        </div>
+
+        <div class="pl-4 flex-grow relative">
+          <p>
+            <a :href="tweet.user.profile" target="_blank" class="no-underline" @click.stop>
+              {{ tweet.user.name }}
+            </a><br>
+            <a :href="tweet.user.profile" target="_blank" class="no-underline text-grey-dark" @click.stop>
+              {{ tweet.user.handle }}
+            </a>
+          </p>
+        </div>
+
+        <div class="pl-4 flex-none relative">
+          <a :href="baseLink" target="_blank" @click.stop>
+            <i class="fab fa-twitter text-3xl text-white" @click.stop></i>
+          </a>
+        </div>
+      </div>
+
+      <div class="pt-4 flex flex-row relative">
+        <p>{{ tweet.blurb }}</p>
+      </div>
+
+      <div class="pt-4 flex flex-row relative">
+        <div class="flex-grow">
+          <p class="text-sea-green">
+            <i class="far fa-comment hover:text-white trans-color"></i>
+            <i class="fas fa-retweet hover:text-white trans-color"></i>
+            <i class="far fa-heart hover:text-white trans-color"></i>
+          </p>
+        </div>
+
+        <div class="pl-4 flex-none relative">
+          <p class="text-sea-green" @click.stop>26 Apr 2018</p>
+        </div>
+      </div>
+
+    </card>
+
+  </div>
+</template>
+<script>
+import Card from './Card.vue';
+
+export default {
+  name: "twitter-card",
+  components: {
+    Card
+  },
+  props: {
+    tweet
+  }
+}
+</script>
