eaf56edebb68a47899b95b6da453e90dbea66fc9
Elliot Derhay <e.j.derhay@gmail.com>
2018-07-27 11:46:10 -0400

    A good bit of Tweet markup changes + minor design updates
    Specifics:
    - Most important change: stop click handler on tweet link elements
    - Reformat dummy tweet data

--- resources/assets/js/TwitterApp.vue
+++ resources/assets/js/TwitterApp.vue
@@ -4,46 +4,46 @@
     <timeline>
       <card v-for="(tweet, index) in tweets" :size="'sm'" :url="tweet.link" :key="`tweet-${index}`">
 
-        <div class="flex flex-row">
+        <div class="flex flex-row relative">
           <div>
-            <a :href="tweet.link" target="_blank">
-              <img width="48" height="48" :src="tweet.icon" class="border-solid border-2 border-white rounded-full">
+            <a :href="tweet.user.profile" target="_blank" @click.stop>
+              <img width="48" height="48" :src="tweet.user.avatar" class="border-solid border-2 border-white rounded-full" @click.stop>
             </a>
           </div>
 
-          <div class="pl-4 flex-grow">
+          <div class="pl-4 flex-grow relative">
             <p>
-              <a :href="tweet.link" target="_blank" class="no-underline">
-                {{ tweet.name }}
+              <a :href="tweet.user.profile" target="_blank" class="no-underline" @click.stop>
+                {{ tweet.user.name }}
               </a><br>
-              <a :href="tweet.link" target="_blank" class="no-underline text-grey-dark">
-                {{ tweet.username }}
+              <a :href="tweet.user.profile" target="_blank" class="no-underline text-grey-dark" @click.stop>
+                {{ tweet.user.handle }}
               </a>
             </p>
           </div>
 
-          <div class="pl-4 flex-none">
-            <a href="https://twitter.com">
-              <i class="fab fa-twitter text-4xl text-white"></i>
+          <div class="pl-4 flex-none relative">
+            <a :href="baseLink" target="_blank" @click.stop>
+              <i class="fab fa-twitter text-3xl text-white" @click.stop></i>
             </a>
           </div>
         </div>
 
-        <div class="pt-4 flex flex-row">
+        <div class="pt-4 flex flex-row relative">
           <p>{{ tweet.blurb }}</p>
         </div>
 
-        <div class="pt-4 flex flex-row">
+        <div class="pt-4 flex flex-row relative">
           <div class="flex-grow">
             <p class="text-sea-green">
-              <i class="far fa-comment"></i>
-              <i class="fas fa-retweet"></i>
-              <i class="far fa-heart"></i>
+              <i class="far fa-comment hover:text-white trans-color"></i>
+              <i class="fas fa-retweet hover:text-white trans-color"></i>
+              <i class="far fa-heart hover:text-white trans-color"></i>
             </p>
           </div>
 
-          <div class="pl-4 flex-none">
-            <p class="text-sea-green">26 Apr 2018</p>
+          <div class="pl-4 flex-none relative">
+            <p class="text-sea-green" @click.stop>26 Apr 2018</p>
           </div>
         </div>
 
@@ -67,13 +67,17 @@ export default {
   data: () => ({
     tweets: [
       {
-        icon: 'https://pbs.twimg.com/profile_images/936031034168172545/TwJzUf5p_bigger.jpg',
-        name: 'Elliot Derhay',
-        username: '@JSn1nj4',
+        user: {
+          avatar: 'https://pbs.twimg.com/profile_images/936031034168172545/TwJzUf5p_bigger.jpg',
+          name: 'Elliot Derhay',
+          handle: '@JSn1nj4',
+          profile: 'https://twitter.com/JSn1nj4/'
+        },
         blurb: 'Twitter has some nice keyboard shortcuts. \`Shift+?\`',
         link: 'https://twitter.com/JSn1nj4/status/989678620598853635'
       }
-    ]
+    ],
+    baseLink: 'https://twitter.com/'
   })
 
 }
