8784b32287d5935142a7871c7e15a96a0ae75780
JSn1nj4 <e.j.derhay@gmail.com>
2018-08-25 18:05:18 -0400

    Finish LoadingAnimation `fadeIn` and `fadeOut` methods
    I'm not 100% sure I'll need the `fadeIn` method for the LoadingAnimation, but I figured having a method that undoes the `fadeOut` in reverse could be handy.

--- resources/assets/js/components/LoadingAnimation.vue
+++ resources/assets/js/components/LoadingAnimation.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="`loading-animation ${this.show ? 'show' : 'hide'}`">
+  <div :class="`loading-animation ${this.show ? 'show' : 'hide'} z-${zIndex}`">
     <socket size="80px" background="#00c49a"></socket>
   </div>
 </template>
@@ -13,20 +13,37 @@ export default {
   },
 
   data: () => ({
-    show: true
+    show: true,
+    zIndex: '10'
   }),
 
   methods: {
     fadeOut() {
-      alert('Fading out');
       this.show = false;
+      setTimeout(function() {
+        this.zIndex = '0';
+        console.log("zIndex: " + this.zIndex);
+      }, 1000);
+    },
+
+    fadeIn() {
+      this.zIndex = '10';
+      this.show = true;
     }
   }
 }
 </script>
 <style lang="scss">
-.loading-animation.hide {
-  display: none;
+.loading-animation {
+  transition: opacity 1s;
+
+  &.show {
+    opacity: 1;
+  }
+
+  &.hide {
+    opacity: 0;
+  }
 }
 .hex-brick {
   animation-name: socket-fade !important;
