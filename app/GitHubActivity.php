c1de3e0b2a54980dc075a1b856a1b79321d53580
Elliot Derhay <e.j.derhay@gmail.com>
2018-08-29 12:04:19 -0400

    Begin building out GitHubActivity model
    Changes:
    - Add base API URL property
    - Add GitHub token property
    - Add constructor function for overriding parent constructor
    
    This class's constructor override's the parent constructor so that the token property can be set. But it still calls the parent's constructor for the sake of inheritance.

--- app/GitHubActivity.php
+++ app/GitHubActivity.php
@@ -6,5 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class GitHubActivity extends Model
 {
-    //
+    /**
+     * The base GitHub API URL
+     *
+     * @var string
+     */
+    private $api_url = 'https://api.github.com';
+
+    /**
+     * The token used for retrieving information from the GitHub API
+     *
+     * @var string
+     */
+    private $token;
+
+    /**
+     * Create a new instance of the GitHubActivity model
+     *
+     * @param array     $attributes
+     * @return void
+     *
+     * This is necessary to initialize some properties that can't otherwise be
+     * initialized. Initializing properties outside of a constrctor requires
+     * that the initial values be static.
+     */
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        $this->token = config('service.github.token', false);
+    }
 }
