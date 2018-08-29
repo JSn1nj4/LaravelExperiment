0890c3ae0898cc90341b3199a315768898bcffc3
Elliot Derhay <e.j.derhay@gmail.com>
2018-08-29 12:14:23 -0400

    Rename a controller property and make first call to model

--- app/Http/Controllers/GitHubActivityController.php
+++ app/Http/Controllers/GitHubActivityController.php
@@ -6,15 +6,15 @@ use App\GitHubActivity;
 
 class GitHubActivityController extends Controller
 {
-    private $GitHubActivity;
+    private $github_activity;
 
     public function __construct()
     {
-        $this->GitHubActivity = new GitHubActivity;
+        $this->github_activity = new GitHubActivity;
     }
 
     public function index(int $count = 5)
     {
-        return ['message' => 'test'];
+        return $this->github_activity->getRawActivity( "$this->github_activity->api_url/users/JSn1nj4/events" );
     }
 }
