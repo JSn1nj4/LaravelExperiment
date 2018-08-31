2a4ec32369eca3f0d53d1524cbf7927216931931
JSn1nj4 <e.j.derhay@gmail.com>
2018-08-30 22:32:35 -0400

    Add some documentation to the GH activity controller

--- app/Http/Controllers/GitHubActivityController.php
+++ app/Http/Controllers/GitHubActivityController.php
@@ -6,13 +6,34 @@ use App\GitHubActivity;
 
 class GitHubActivityController extends Controller
 {
+    /**
+     * Instance of GitHubActivity model
+     *
+     * @property object     $github_activity
+     */
     private $github_activity;
 
+    /**
+     * GitHubActivityController constructor method
+     *
+     * @method              __construct
+     * @return void
+     */
     public function __construct()
     {
         $this->github_activity = new GitHubActivity;
     }
 
+    /**
+     * Index recent GitHub activity events
+     *
+     * @method              index
+     * @param int           $count
+     * @return array
+     *
+     * This will return an asociative array that should automatically be
+     * converted to JSON on the front-end.        
+     */
     public function index(int $count = 5)
     {
         return $this->github_activity->getActivity($count);
