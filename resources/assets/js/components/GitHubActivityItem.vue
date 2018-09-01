ebed85f81baaa81cf895d28138d75d7c12bda756
JSn1nj4 <e.j.derhay@gmail.com>
2018-09-01 18:43:45 -0400

    Setup dummy GitHub Activity Item markup
    This is for visualizing what will be displayed according to what data is given to the component (since there will be multiple types of GitHub events).
    
    This first set of dummy markup is for push events. Others will be built out once the basics of these events on the timeline are built out.

--- resources/assets/js/components/GitHubActivityItem.vue
+++ resources/assets/js/components/GitHubActivityItem.vue
@@ -2,7 +2,33 @@
   <div class="github-activity-item">
     <card :size="'sm'">
 
-      <!--  -->
+      <div class="flex flex-row relative">
+        <div class="text-grey fab arrow-alt-circle-up"></div>
+
+        <div class="pl-4 flex-grow relative">
+          <p class="text-grey">
+            about X hours ago
+          </p>
+
+          <p class="font-white">
+            <strong>
+              <a href="#" target="_blank">JSn1nj4</a> pushed to <a href="#" target="_blank">master</a> at <a href="#" target="_blank">JSn1nj4/ElliotDerhay.com</a>
+            </strong>
+          </p>
+
+          <!-- Repeat for each commit pushed -->
+          <p class="font-grey">
+            <a href="#" target="_blank" class="no-underline font-bold">
+              <img width="16" height="16" src="https://pbs.twimg.com/profile_images/936031034168172545/TwJzUf5p_normal.jpg" class="inline-block">
+            </a> <a href="#" target="_blank" class="no-underline">ffffff</a> Lorem ipsum dolor sit amet
+          </p>
+          <p class="font-grey">
+            <a href="#" target="_blank" class="no-underline font-bold">
+              <img width="24" height="24" src="https://pbs.twimg.com/profile_images/936031034168172545/TwJzUf5p_normal.jpg">
+            </a> <a href="#" target="_blank" class="no-underline">ffffff</a> Lorem ipsum dolor sit amet
+          </p>
+        </div>
+      </div>
 
     </card>
   </div>
