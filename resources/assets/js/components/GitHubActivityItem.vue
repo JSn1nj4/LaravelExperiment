0883ec9b697d5ffb618ec94adc030d2e7c7a736f
JSn1nj4 <e.j.derhay@gmail.com>
2018-09-01 15:45:32 -0400

    Begin building GitHub activity components

---
+++ resources/assets/js/components/GitHubActivityItem.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <div class="github-activity-item">
+    <card :size="'sm'">
+
+      <!--  -->
+
+    </card>
+  </div>
+</template>
+
+<script>
+import moment from 'moment';
+import Card from './Card.vue';
+
+export default {
+  name: "github-activity-item",
+  components: {
+    Card
+  },
+  props: {
+    'github-activity': Object
+  },
+  data: () => ({
+    baseLink: 'https://github.com'
+  }),
+  mounted() {
+    console.log(this['github-activity']);
+  }
+}
+</script>
