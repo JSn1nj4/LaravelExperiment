16a5f575cd79f3d607221d5ec2164120a9f19f98
JSn1nj4 <e.j.derhay@gmail.com>
2018-09-03 18:00:23 -0400

    Reformat template for better event message rendering
    This allows the message selection to be more automatic. It's not 100% ideal, but it's better than creating nearly duplicate sets of markup for the various messages from Create, Push and Delete events.
    
    Doing this did require me to extract the sentence parts into objects containing lists of prepositions and actions used in each event summary, with duplicate field names. Will look later to see if there's a better way to do this.
    
    This -- and a conditional technique in Vue using `<template>` elements -- also allowed me to remove the wrapper div from around body of the rendered event.
    
    Additionally, add new `branchName` and `branchUrl` properties for building the name of the branch and its related URL for the GitHub repo, and remove the `mounted` method since it shouldn't be needed in the future.

--- resources/assets/js/components/GitHubActivityItem.vue
+++ resources/assets/js/components/GitHubActivityItem.vue
@@ -10,37 +10,19 @@
             about {{ formattedDate }}
           </p>
 
-          <div v-if="event.type == 'CreateEvent'">
-            <p class="font-white mt-1">
-              <strong>
-                <a href="#" target="_blank" class="no-underline">JSn1nj4</a> pushed to <a href="#" target="_blank" class="no-underline">master</a> at <a href="#" target="_blank" class="no-underline">JSn1nj4/ElliotDerhay.com</a>
-              </strong>
-            </p>
-          </div>
+          <p class="font-white mt-1">
+            <strong>
+              <a href="#" target="_blank" class="no-underline">{{ event.actor.display_login }}</a> {{ actions[event.type] }} <a :href="branchUrl" target="_blank" class="no-underline">{{ branchName }}</a> {{ prepositions[event.type] }} <a href="#" target="_blank" class="no-underline">{{ event.repo.name }}</a>
+            </strong>
+          </p>
 
-          <div v-if="event.type == 'PushEvent'">
-            <p class="font-white mt-1">
-              <strong>
-                <a href="#" target="_blank" class="no-underline">JSn1nj4</a> pushed to <a href="#" target="_blank" class="no-underline">master</a> at <a href="#" target="_blank" class="no-underline">JSn1nj4/ElliotDerhay.com</a>
-              </strong>
-            </p>
-
-            <!-- Repeat for each commit pushed -->
+          <template v-if="event.type == 'PushEvent'">
             <p v-for="commit in event.payload.commits" :key="commit.sha" class="font-grey align-middle mt-1">
               <a href="#" target="_blank" class="no-underline font-bold">
-                <img width="18" height="18" class="align-bottom" src="https://pbs.twimg.com/profile_images/936031034168172545/TwJzUf5p_normal.jpg">
+                <img width="18" height="18" class="align-bottom" :src="tmpAvatarUrl">
               </a> <a href="#" target="_blank" class="no-underline">{{ shortHash(commit.sha) }}</a> {{ shortMsg(commit.message) }}
             </p>
-          </div>
-
-          <div v-if="event.type == 'DeleteEvent'">
-            <p class="font-white mt-1">
-              <strong>
-                <a href="#" target="_blank" class="no-underline">JSn1nj4</a> deleted <a href="#" target="_blank" class="no-underline">master</a> from <a href="#" target="_blank" class="no-underline">JSn1nj4/ElliotDerhay.com</a>
-              </strong>
-            </p>
-          </div>
-
+          </template>
 
         </div>
       </div>
@@ -64,14 +46,31 @@ export default {
   data: () => ({
     baseLink: 'https://github.com',
     icons: {
-      'CreateEvent': 'far fa-plus-square',
-      'PushEvent': 'far fa-arrow-alt-circle-up',
-      'DeleteEvent': 'far fa-trash-alt'
-    }
+      CreateEvent: 'far fa-plus-square',
+      PushEvent: 'far fa-arrow-alt-circle-up',
+      DeleteEvent: 'far fa-trash-alt'
+    },
+    tmpAvatarUrl: 'https://pbs.twimg.com/profile_images/936031034168172545/TwJzUf5p_normal.jpg',
+    prepositions: {
+      CreateEvent: 'at',
+      PushEvent: 'at',
+      DeleteEvent: 'from'
+    },
+    actions: {
+      CreateEvent: 'created',
+      PushEvent: 'pushed to',
+      DeleteEvent: 'deleted'
+    },
   }),
   computed: {
     formattedDate() {
       return moment(this.event.created_at).fromNow();
+    },
+    branchName() {
+      return this.event.payload.ref.replace('refs/heads/', '');
+    },
+    branchUrl() {
+      return `${this.event.repo.url}/tree/${this.branchName}`;
     }
   },
   methods: {
@@ -82,9 +81,6 @@ export default {
     shortHash(hash) {
       return hash.slice(0, 6);
     }
-  },
-  mounted() {
-    console.log(this['github-activity']);
   }
 }
 </script>
