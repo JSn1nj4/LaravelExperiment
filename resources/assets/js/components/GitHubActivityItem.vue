8b5d69289825f95689edf1960c4f23db2dcec1de
JSn1nj4 <e.j.derhay@gmail.com>
2018-09-03 11:01:08 -0400

    Use different test markup for Create, Delete and Push events
    Also use different icons for each event type

--- resources/assets/js/components/GitHubActivityItem.vue
+++ resources/assets/js/components/GitHubActivityItem.vue
@@ -3,30 +3,45 @@
     <card :size="'sm'" type="transparent">
 
       <div class="flex flex-row relative">
-        <div class="text-grey far fa-arrow-alt-circle-up"></div>
+        <div class="text-grey" :class="icons[event.type]"></div>
 
         <div class="pl-4 flex-grow relative">
           <p class="text-grey">
             about X hours ago
           </p>
 
-          <p class="font-white mt-1">
-            <strong>
-              <a href="#" target="_blank" class="no-underline">JSn1nj4</a> pushed to <a href="#" target="_blank" class="no-underline">master</a> at <a href="#" target="_blank" class="no-underline">JSn1nj4/ElliotDerhay.com</a>
-            </strong>
-          </p>
+          <div v-if="event.type == 'CreateEvent'">
+            <p class="font-white mt-1">
+              <strong>
+                <a href="#" target="_blank" class="no-underline">JSn1nj4</a> pushed to <a href="#" target="_blank" class="no-underline">master</a> at <a href="#" target="_blank" class="no-underline">JSn1nj4/ElliotDerhay.com</a>
+              </strong>
+            </p>
+          </div>
 
-          <!-- Repeat for each commit pushed -->
-          <p class="font-grey align-middle mt-1">
-            <a href="#" target="_blank" class="no-underline font-bold">
-              <img width="18" height="18" class="align-bottom" src="https://pbs.twimg.com/profile_images/936031034168172545/TwJzUf5p_normal.jpg">
-            </a> <a href="#" target="_blank" class="no-underline">ffffff</a> Lorem ipsum dolor sit amet
-          </p>
-          <p class="font-grey align-middle mt-1">
-            <a href="#" target="_blank" class="no-underline font-bold">
-              <img width="18" height="18" class="align-bottom" src="https://pbs.twimg.com/profile_images/936031034168172545/TwJzUf5p_normal.jpg">
-            </a> <a href="#" target="_blank" class="no-underline">ffffff</a> Lorem ipsum dolor sit amet
-          </p>
+          <div v-if="event.type == 'PushEvent'">
+            <p class="font-white mt-1">
+              <strong>
+                <a href="#" target="_blank" class="no-underline">JSn1nj4</a> pushed to <a href="#" target="_blank" class="no-underline">master</a> at <a href="#" target="_blank" class="no-underline">JSn1nj4/ElliotDerhay.com</a>
+              </strong>
+            </p>
+
+            <!-- Repeat for each commit pushed -->
+            <p v-for="commit in event.payload.commits" :key="commit.sha" class="font-grey align-middle mt-1">
+              <a href="#" target="_blank" class="no-underline font-bold">
+                <img width="18" height="18" class="align-bottom" src="https://pbs.twimg.com/profile_images/936031034168172545/TwJzUf5p_normal.jpg">
+              </a> <a href="#" target="_blank" class="no-underline">ffffff</a> Lorem ipsum dolor sit amet
+            </p>
+          </div>
+
+          <div v-if="event.type == 'DeleteEvent'">
+            <p class="font-white mt-1">
+              <strong>
+                <a href="#" target="_blank" class="no-underline">JSn1nj4</a> deleted <a href="#" target="_blank" class="no-underline">master</a> from <a href="#" target="_blank" class="no-underline">JSn1nj4/ElliotDerhay.com</a>
+              </strong>
+            </p>
+          </div>
+
+
         </div>
       </div>
 
@@ -47,7 +62,12 @@ export default {
     event: Object
   },
   data: () => ({
-    baseLink: 'https://github.com'
+    baseLink: 'https://github.com',
+    icons: {
+      'CreateEvent': 'far fa-plus-square',
+      'PushEvent': 'far fa-arrow-alt-circle-up',
+      'DeleteEvent': 'far fa-trash-alt'
+    }
   }),
   mounted() {
     console.log(this['github-activity']);
