83338634f71b164fa265dc4ac18a9aab0eb8e93f
JSn1nj4 <e.j.derhay@gmail.com>
2018-09-03 22:58:26 -0400

    Style and link various parts of component
    Changes:
    - Reformat markup to make it more readable
    - Add computed properties and a new method for generating various URLs
    - Fix how branch URLs are generated
    - Invert event-related data organization
    - Style timeline marker icons
    
    The second-to-last item refers to taking the icons, actions, and prepositions properties and inverting how they're structured. Now there will be a property of strings for each type of event instead.
    
    I'll have to see about a better way to handle that, but this makes it easier to allow multiple types of events to share some strings -- particularly, "action" and "preposition" words used in each event summary.
    
    Also, the branch URL computed property needed to be fixed; clicking the links created using these URLs would bring the user to the related GitHub API endpoint rather than the actual page on GitHub for the branch. This now correctly brings the user to the tree view for the branch on GitHub.

--- resources/assets/js/components/GitHubActivityItem.vue
+++ resources/assets/js/components/GitHubActivityItem.vue
@@ -3,7 +3,7 @@
     <card :size="'sm'" type="transparent">
 
       <div class="flex flex-row relative">
-        <div class="text-grey" :class="icons[event.type]"></div>
+        <div class="text-grey text-center flex-none github-activity-icon" :class="this[event.type].icon" style="width: 48px; font-size: 22px;"></div>
 
         <div class="pl-4 flex-grow relative">
           <p class="text-grey">
@@ -12,15 +12,36 @@
 
           <p class="font-white mt-1">
             <strong>
-              <a href="#" target="_blank" class="no-underline">{{ event.actor.display_login }}</a> {{ actions[event.type] }} <a :href="branchUrl" target="_blank" class="no-underline">{{ branchName }}</a> {{ prepositions[event.type] }} <a href="#" target="_blank" class="no-underline">{{ event.repo.name }}</a>
+              <a :href="profileUrl" target="_blank" class="no-underline">
+                {{ event.actor.display_login }}
+              </a>
+
+              {{ this[event.type].action }}
+
+              <span v-if="event.type == 'DeleteEvent'" class="no-underline text-sea-green-darker">
+                {{ branchName }}
+              </span>
+              <a v-else :href="branchUrl" target="_blank" class="no-underline">
+                {{ branchName }}
+              </a>
+
+              {{ this[event.type].preposition }}
+
+              <a :href="repoUrl" target="_blank" class="no-underline">
+                {{ event.repo.name }}
+              </a>
             </strong>
           </p>
 
           <template v-if="event.type == 'PushEvent'">
             <p v-for="commit in event.payload.commits" :key="commit.sha" class="font-grey align-middle mt-1">
-              <a href="#" target="_blank" class="no-underline font-bold">
+              <a :href="profileUrl" target="_blank" class="no-underline font-bold">
                 <img width="18" height="18" class="align-bottom" :src="tmpAvatarUrl">
-              </a> <a href="#" target="_blank" class="no-underline">{{ shortHash(commit.sha) }}</a> {{ shortMsg(commit.message) }}
+              </a>
+              <a :href="commitUrl(commit.sha)" target="_blank" class="no-underline">
+                {{ shortHash(commit.sha) }}
+              </a>
+              {{ shortMsg(commit.message) }}
             </p>
           </template>
 
@@ -45,32 +66,38 @@ export default {
   },
   data: () => ({
     baseLink: 'https://github.com',
-    icons: {
-      CreateEvent: 'far fa-plus-square',
-      PushEvent: 'far fa-arrow-alt-circle-up',
-      DeleteEvent: 'far fa-trash-alt'
+    CreateEvent: {
+      icon: 'far fa-plus-square',
+      action: 'created',
+      preposition: 'at'
+    },
+    PushEvent: {
+      icon: 'far fa-arrow-alt-circle-up',
+      action: 'pushed to',
+      preposition: 'at'
+    },
+    DeleteEvent: {
+      icon: 'far fa-trash-alt',
+      action: 'deleted',
+      preposition: 'from'
     },
     tmpAvatarUrl: 'https://pbs.twimg.com/profile_images/936031034168172545/TwJzUf5p_normal.jpg',
-    prepositions: {
-      CreateEvent: 'at',
-      PushEvent: 'at',
-      DeleteEvent: 'from'
-    },
-    actions: {
-      CreateEvent: 'created',
-      PushEvent: 'pushed to',
-      DeleteEvent: 'deleted'
-    },
   }),
   computed: {
     formattedDate() {
       return moment(this.event.created_at).fromNow();
     },
+    profileUrl() {
+      return `${this.baseLink}/${this.event.actor.login}`;
+    },
+    repoUrl() {
+      return `${this.baseLink}/${this.event.repo.name}`;
+    },
     branchName() {
       return this.event.payload.ref.replace('refs/heads/', '');
     },
     branchUrl() {
-      return `${this.event.repo.url}/tree/${this.branchName}`;
+      return `${this.repoUrl}/tree/${this.branchName}`;
     }
   },
   methods: {
@@ -80,7 +107,17 @@ export default {
     },
     shortHash(hash) {
       return hash.slice(0, 6);
+    },
+    commitUrl(hash) {
+      return `${this.repoUrl}/commit/${hash}`;
     }
   }
 }
 </script>
+<style lang="scss">
+  .github-activity-icon {
+    &:before {
+      background: #151515;
+    }
+  }
+</style>
