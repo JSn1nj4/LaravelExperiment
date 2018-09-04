9bc0283c2b966a8cd4eba689521d06328e2ff38e
JSn1nj4 <e.j.derhay@gmail.com>
2018-09-03 23:08:59 -0400

    Note where design of activity feed gets inspiration from

--- resources/assets/js/GitHubActivityTimelineWidget.js
+++ resources/assets/js/GitHubActivityTimelineWidget.js
@@ -1,3 +1,8 @@
+/**
+ * Activity item design inspiration:
+ * - https://github.com/caseyscarborough/github-activity
+ */
+
 import GitHubActivityFeed from './GitHubActivityFeed.vue';
 
 const GitHubActivityFeedWidget = new Vue({
