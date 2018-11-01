29f34c7fba6ae52038199de8b67b218ca40eec2d
Elliot Derhay <e.j.derhay@gmail.com>
2018-10-31 23:32:20 -0400

    Clean up IssuesEvent component

--- resources/assets/js/components/GitHubActivityTypes/IssuesEvent.vue
+++ resources/assets/js/components/GitHubActivityTypes/IssuesEvent.vue
@@ -1,68 +1,54 @@
 <template lang="html">
-  <div class="github-activity-item">
-    <card :size="'sm'" type="transparent">
+  <div class="flex flex-row relative">
+    <div class="text-grey text-center flex-none github-activity-icon" :class="this.icon" style="width: 2rem; font-size: 22px;"></div>
 
-      <div class="flex flex-row relative">
-        <div class="text-grey text-center flex-none github-activity-icon" :class="this.icon" style="width: 2rem; font-size: 22px;"></div>
+    <div class="pl-4 flex-grow relative">
+      <p class="text-grey">
+        about {{ formattedDate }}
+      </p>
 
-        <div class="pl-4 flex-grow relative">
-          <p class="text-grey">
-            about {{ formattedDate }}
-          </p>
+      <p class="font-white mt-1 text-sm">
+        <strong>
+          <a :href="profileUrl" target="_blank" class="no-underline">
+            {{ event.actor.display_login }}
+          </a>
 
-          <p class="font-white mt-1 text-sm">
-            <strong>
-              <a :href="profileUrl" target="_blank" class="no-underline">
-                {{ event.actor.display_login }}
-              </a>
+          {{ this.action }}
 
-              {{ this.action }}
+          <a :href="this.event.payload.issue.html_url" target="_blank" class="no-underline text-sea-green">
+            {{ issueNumberString }}
+          </a>
 
-              <a :href="this.event.payload.issue.html_url" target="_blank" class="no-underline text-sea-green">
-                {{ issueNumberString }}
-              </a>
+          {{ this.preposition }}
 
-              {{ this.preposition }}
+          <a :href="repoUrl" target="_blank" class="no-underline">
+            {{ event.repo.name }}
+          </a>
+        </strong>
+      </p>
 
-              <a :href="repoUrl" target="_blank" class="no-underline">
-                {{ event.repo.name }}
-              </a>
-            </strong>
-          </p>
+      <p class="font-grey align-middle mt-2">
+        <a :href="event.payload.issue.user.html_url">
+          <img width="18" height="18" class="align-bottom" :src="event.payload.issue.user.avatar_url">
+        </a>
 
-          <p class="font-grey align-middle mt-2">
-            <a :href="event.payload.issue.user.html_url">
-              <img width="18" height="18" class="align-bottom" :src="event.payload.issue.user.avatar_url">
-            </a>
+        {{ event.payload.issue.title }}
+      </p>
 
-            {{ event.payload.issue.title }}
-          </p>
-
-        </div>
-      </div>
-
-    </card>
+    </div>
   </div>
 </template>
 
 <script>
-import moment from 'moment';
-import Card from './Card.vue';
+import GitHubActivityMixin from '../../mixins/GitHubActivity';
 
 export default {
-  name: "git-hub-activity-item",
-  components: {
-    Card
-  },
-  props: {
-    event: Object
-  },
+  name: "git-hub-issues-event",
+  mixins: [GitHubActivityMixin],
   data: () => ({
-    baseLink: 'https://github.com',
     icon: 'far fa-file-alt',
     action: 'opened',
-    preposition: 'at',
-    tmpAvatarUrl: 'https://pbs.twimg.com/profile_images/936031034168172545/TwJzUf5p_normal.jpg',
+    preposition: 'at'
   }),
   computed: {
     formattedDate() {
@@ -79,20 +65,11 @@ export default {
     },
   },
   mounted() {
-    if(this.event.type == 'IssuesEvent') {
-      this.IssuesEvent.action = this.event.payload.action;
-    }
+    this.IssuesEvent.action = this.event.payload.action;
 
-    if(this.event.type == 'IssuesEvent' && this.event.payload.action == 'closed') {
+    if(this.event.payload.action == 'closed') {
       this.IssuesEvent.icon = 'fas fa-minus-circle';
     }
   }
 }
 </script>
-<style lang="scss">
-  .github-activity-icon {
-    &:before {
-      background: #151515;
-    }
-  }
-</style>
