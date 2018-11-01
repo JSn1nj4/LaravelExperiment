f97d286758ea377d53408a088b913123955330c9
Elliot Derhay <e.j.derhay@gmail.com>
2018-10-31 23:02:20 -0400

    Add GitHubActivityWrapper for some reusable markup

---
+++ resources/assets/js/components/GitHubActivityWrapper.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div class="github-activity-item">
+    <card :size="'sm'" type="transparent">
+
+      <div class="flex flex-row relative">
+
+        <slot></slot>
+
+      </div>
+
+    </card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "git-hub-activity-wrapper"
+}
+</script>
