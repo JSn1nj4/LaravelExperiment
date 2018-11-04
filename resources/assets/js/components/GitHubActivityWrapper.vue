9d5508dc8f98c9b015c858524b3810c6c07229b0
JSn1nj4 <e.j.derhay@gmail.com>
2018-11-03 21:58:54 -0500

    Move dynamic import responsibility to wrapper component
    This makes it so that the required components don't have to be loaded up front by the timeline itself.
    
    It also makes it simpler to load a single component, which is selected dynamically.
    
    Based on this [solution][1].
    
    [1]: https://medium.com/scrumpy/dynamic-component-templates-with-vue-js-d9236ab183bb

--- resources/assets/js/components/GitHubActivityWrapper.vue
+++ resources/assets/js/components/GitHubActivityWrapper.vue
@@ -2,7 +2,7 @@
   <div class="github-activity-item">
     <card :size="'sm'" type="transparent">
 
-      <slot></slot>
+      <component :is="component" :event="event" v-if="component"></component>
 
     </card>
   </div>
@@ -13,8 +13,35 @@ import Card from './Card.vue';
 
 export default {
   name: "git-hub-activity-wrapper",
+  props: {
+    event: Object
+  },
+
   components: {
     Card
+  },
+
+  data: () => ({
+    component: null
+  }),
+
+  computed: {
+    loader() {
+      if(!this.event.type) {
+        return null;
+      }
+      return () => import(`./GitHubActivityTypes/${this.event.type}.vue`);
+    }
+  },
+
+  mounted() {
+    this.loader()
+      .then(() => {
+        this.component = () => this.loader();
+      })
+      .catch((error) => {
+        console.log(`Unable to import component. Error: ${error}`);
+      });
   }
 }
 </script>
