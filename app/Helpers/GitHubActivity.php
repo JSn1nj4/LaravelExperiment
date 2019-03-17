46c8a9f2c373fadb56dc8b7fad56e04b76fea41e
Elliot Derhay <e.j.derhay@gmail.com>
2019-03-17 18:12:29 -0400

    Create helper classes and add to autoloader
    These helper classes will contain static methods for helping with Github activity items and tweets.

---
+++ app/Helpers/GitHubActivity.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Helpers;
+
+/**
+ * Used to help with rendering GitHub activity items server-side
+ */
+class GitHubActivity
+{
+    //
+}
+
+?>
---
+++ app/Helpers/Tweet.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Helpers;
+
+/**
+ *  Used for working with Twitter timeline items rendered server-side
+ */
+class Tweet
+{
+    //
+}
+
+?>
