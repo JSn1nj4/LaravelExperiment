562553b8bd912d6b72776846601229b32266e979
Elliot Derhay <e.j.derhay@gmail.com>
2019-03-18 14:47:30 -0400

    Add placeholder Tweet helper methods
    Will need to finish fleshing out these methods. For now, they're mostly there as placeholders.

--- app/Helpers/Tweet.php
+++ app/Helpers/Tweet.php
@@ -7,7 +7,59 @@ namespace App\Helpers;
  */
 class Tweet
 {
-    //
+    /**
+     * @param string                        $date
+     *
+     * @return string
+     *
+     * Format a date from a given date string
+     */
+    public static function formatDate(string $date)
+    {
+        // format date
+
+        return $date;
+    }
+
+    /**
+     * @param string                        $baseLink
+     * @param string                        $screen_name
+     *
+     * @return string
+     *
+     * Return a valid Twitter profile URL
+     */
+    public static function profileUrl(string $baseLink, string $screen_name)
+    {
+        return "$baseLink/$screen_name";
+    }
+
+    /**
+     * @param string                        $profileUrl
+     * @param string                        $id
+     *
+     * @return string
+     *
+     * Return a valid Tweet URL
+     */
+    public static function tweetUrl(string $profileUrl, string $id)
+    {
+        return "$profileUrl/status/$id";
+    }
+
+    /**
+     * @param string                        $text
+     *
+     * @return void
+     *
+     * Render a given tweet body as HTML
+     */
+    public static function formatBody(string $text)
+    {
+        // HTML formatting
+
+        return $text; // TEMP: see method description
+    }
 }
 
 ?>
