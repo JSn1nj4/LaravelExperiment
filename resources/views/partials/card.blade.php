0a2482fa676e23dd35b1495c1dbe1050ec7b835a
Elliot Derhay <e.j.derhay@gmail.com>
2019-03-28 18:38:09 -0400

    Fix format of `$typeClasses` array
    Forgot to adjust the format of this data after copying it from a Vue component.

--- resources/views/partials/card.blade.php
+++ resources/views/partials/card.blade.php
@@ -3,8 +3,8 @@
     $type = !empty($type) ? $type : 'default';
 
     $typeClasses = [
-        'default': 'p-4 rounded-lg border border-grey-dark trans-border-color hover:border-sea-green bg-black',
-        'transparent': 'px-4'
+        'default' => 'p-4 rounded-lg border border-grey-dark trans-border-color hover:border-sea-green bg-black',
+        'transparent' => 'px-4'
     ];
 
     $classes = "relative my-4 max-w-$size w-full z-30 $typeClasses[$type]"
