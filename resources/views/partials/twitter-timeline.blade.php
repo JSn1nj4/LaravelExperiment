e4d702133f35013f671d7636c56561a379d7d1b6
Elliot Derhay <e.j.derhay@gmail.com>
2019-03-28 18:43:10 -0400

    Finish creating twitter-timeline partial
    This partial serves to request tweets and load them into the `timeline` partial.

---
+++ resources/views/partials/twitter-timeline.blade.php
@@ -0,0 +1,18 @@
+@php
+    use App\Http\Controllers\TweetController;
+
+    $count = !empty($count) ? (int) $count : 5;
+    $loaderSize = !empty($loaderSize) ? $loaderSize : '40px';
+
+    $tweets = json_decode((new TweetController)->index($count));
+@endphp
+
+@extends('partials.timeline', [
+    'showLine' => ($count >= 2)
+])
+
+<div class="max-w-sm m-auto mb-4" style="min-height: {{ $loaderSize }}">
+    @section('timeline-content')
+        @each('partials.twitter-card', $tweets, 'tweet')
+    @endsection
+</div>
