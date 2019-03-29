ee2422e2122032a02ef3bcebe868ee13623c65d4
Elliot Derhay <e.j.derhay@gmail.com>
2019-03-29 13:36:51 -0400

    Add GlobalHelpers class with first helper method
    First helper added returns a string representing how long ago something occurred.

---
+++ app/Helpers/GlobalHelpers.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Helpers;
+
+/**
+ * Used as a container for all helpers that will be shared
+ */
+class GlobalHelpers
+{
+    /**
+     * Return a formatted string representing how long ago something happened
+     *
+     * Credit to Glavić on Stack Overflow for the original function this is
+     * based on.
+     * @link: https://stackoverflow.com/a/18602474
+     *
+     * @method          timeElapsedString
+     * @access public
+     * @static
+     *
+     * @param string    $dateString
+     * @param string    $timeZoneString
+     * @param bool      $full
+     *
+     * @return string
+     */
+    public static function timeElapsedString(string $dateString, string $timeZoneString = "America/New_York", $full = false)
+    {
+        $now = new \DateTime;
+        $ago = new \DateTime($datetime, new \DateTimeZone($timeZoneString));
+        $diff = $now->diff($ago);
+
+        $diff->w = floor($diff->d / 7);
+        $diff->d -= $diff->w * 7;
+
+        $string = array(
+            'y' => 'year',
+            'm' => 'month',
+            'w' => 'week',
+            'd' => 'day',
+            'h' => 'hour',
+            'i' => 'minute',
+            's' => 'second',
+        );
+        foreach ($string as $k => &$v) {
+            if ($diff->$k) {
+                $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+            } else {
+                unset($string[$k]);
+            }
+        }
+
+        if (!$full) $string = array_slice($string, 0, 1);
+        return $string ? implode(', ', $string) . ' ago' : 'just now';
+    }
+}
+
+?>
