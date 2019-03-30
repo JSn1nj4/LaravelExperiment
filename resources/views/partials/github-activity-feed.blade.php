1af3cdd44315a4157b0f90e814ee3d29fbd004c3
Elliot Derhay <e.j.derhay@gmail.com>
2019-03-30 15:50:01 -0400

    Build out GitHub Activity Feed template

---
+++ resources/views/partials/github-activity-feed.blade.php
@@ -0,0 +1,14 @@
+@php
+    use App\Http\Controllers\GitHubActivityController;
+
+    $count = !empty($count) ? (int) $count : 7;
+    $loaderSize = !empty($loaderSize) ? $loaderSize : '40px';
+
+    $events = json_decode((new GitHubActivityController)->index($count));
+@endphp
+
+<div class="max-w-sm m-auto mb-4" style="min-height: {{ $loaderSize }};">
+    @component('partials.timeline', ['showLine' => ($count >= 2)])
+        @each('partials.github-activity-card', $events, 'event')
+    @endcomponent
+</div>
