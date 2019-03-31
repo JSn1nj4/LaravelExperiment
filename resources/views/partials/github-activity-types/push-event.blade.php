1f7e41ed16e51a8f76cfce83fb62581c4276c045
Elliot Derhay <e.j.derhay@gmail.com>
2019-03-31 14:27:57 -0400

    Combine 2 `class` attributes on same HTML element
    This was a mistake caused by how the original markup was set up in the Vue template. In Vue, if there's a hard-coded attribute and an attribute binding with the same name, Vue will combine them after evaluating the bound attribute. This obviously won't work in Blade, which was causing the icon to not display at all.

--- resources/views/partials/github-activity-types/push-event.blade.php
+++ resources/views/partials/github-activity-types/push-event.blade.php
@@ -14,7 +14,7 @@
 @endphp
 
 <div class="flex flex-row relative">
-    <div class="text-gray-500 text-center flex-none github-activity-icon" class="{{ $icon }}" style="width: 2rem; font-size: 22px;"></div>
+    <div class="text-gray-500 text-center flex-none github-activity-icon {{ $icon }}" style="width: 2rem; font-size: 22px;"></div>
 
     <div class="pl-4 flex-grow relative">
         <p class="text-gray-500">
