f66f2e5c9271e086412e90613740519ec3dae6df
Elliot Derhay <e.j.derhay@gmail.com>
2019-03-30 23:37:09 -0400

    Build out push-event partial and GitHubActivity helper class
    The GitHubActivity helper class will have more helper methods soon. The ones in there now are matches for the methods and computed properties in the GitHubActivity Vue mixin, and are also what's used by the push-event partial.

---
+++ resources/views/partials/github-activity-types/push-event.blade.php
@@ -0,0 +1,66 @@
+@php
+    use App\Helpers\GitHubActivity as GhHelpers;
+
+    // Settings with common names shared with other activity components
+    $icon = 'far fa-arrow-alt-circle-up';
+    $action = 'pushed to';
+    $preposition = 'at';
+
+    // Values that will be reused multiple times
+    $profileUrl = GhHelpers::profileUrl($event->actor->display_login);
+    $repoUrl = GhHelpers::repoUrl($event->repo->name);
+    $branchName = GhHelpers::branchName($event->payload->ref);
+    $extraCommits = GhHelpers::countExtraCommits($event->payload->commits);
+@endphp
+
+<div class="flex flex-row relative">
+    <div class="text-gray-500 text-center flex-none github-activity-icon" class="{{ $icon }}" style="width: 2rem; font-size: 22px;"></div>
+
+    <div class="pl-4 flex-grow relative">
+        <p class="text-gray-500">
+            {{ GhHelpers::timeElapsedString($event->created_at) }}
+        </p>
+
+        <p class="font-white mt-1 text-sm">
+            <strong>
+                <a href="{{ $profileUrl }}" target="_blank">
+                    {{ $event->actor->display_login }}
+                </a>
+
+                {{ $action }}
+
+                <a href="{{ GhHelpers::branchUrl($repoUrl, $branchName) }}" target="_blank">
+                    {{ $branchName }}
+                </a>
+
+                {{ $preposition }}
+
+                <a href="{{ $repoUrl }}" target="_blank">
+                    {{ $event->repo->name }}
+                </a>
+
+            </strong>
+        </p>
+
+        @foreach(GhHelpers::displayCommits($event->payload->commits) as $commit)
+            <p class="font-gray-500 align-middle mt-2">
+                <a href="{{ $profileUrl }}" target="_blank" class="font-bold">
+                    <img width="18" height="18" class="inline align-bottom" src="{{ GhHelpers::$tmpAvatarUrl }}">
+                </a>
+                <a href="{{ GhHelpers::commitUrl($repoUrl, $commit->sha) }}" target="_blank">
+                    {{ GhHelpers::shortHash($commit->sha) }}
+                </a>
+                {{ GhHelpers::commitSubject($commit->message) }}
+            </p>
+        @endforeach
+
+        @if($extraCommits > 0)
+            <p class="font-gray-500 align-middle mt-2">
+                <a href="{{ GhHelpers::branchCommitsUrl($repoUrl, $branchName) }}" target="_blank" class="font-bold">
+                    +{{ $extraCommits }} more
+                </a>
+            </p>
+        @endif
+
+    </div>
+</div>
