de2162c9724c5deeaea06365b023dec6c35d95a8
Elliot Derhay <e.j.derhay@gmail.com>
2019-03-31 22:36:03 -0400

    Missing semicolon in blade template
    Not that big a deal, but figured it would be good to stay consistent.

--- resources/views/partials/twitter-timeline.blade.php
+++ resources/views/partials/twitter-timeline.blade.php
@@ -7,7 +7,7 @@
     $tweets = json_decode((new TweetController)->index($count));
 @endphp
 
-<div class="max-w-sm m-auto mb-4" style="min-height: {{ $loaderSize }}">
+<div class="max-w-sm m-auto mb-4" style="min-height: {{ $loaderSize }};">
     @component('partials.timeline', ['showLine' => ($count >= 2)])
         @each('partials.twitter-card', $tweets, 'tweet')
     @endcomponent
