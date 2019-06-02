a2b17815fc76153fe848b5754183e487039f08a8
Elliot Derhay <e.j.derhay@gmail.com>
2019-06-02 19:09:12 -0400

    Wrap main ProjectCard content for better formatting
    This wrapper adds a negative margin to offset the Card component's built-in padding.

--- resources/js/components/ProjectCard.vue
+++ resources/js/components/ProjectCard.vue
@@ -3,26 +3,28 @@
 
     <card :size="'md'">
 
-      <a :href="project.demo_link || project.link" :target="project.demo_link ? '_self' : '_blank'">
-        <img :src="project.thumbnail" class="block rounded-lg rounded-b-none">
-      </a>
+      <div class="-m-4">
+        <a :href="project.demo_link || project.link" :target="project.demo_link ? '_self' : '_blank'">
+          <img :src="project.thumbnail" class="block rounded-lg rounded-b-none">
+        </a>
 
-      <div class="p-4">
-        <h4><a :href="project.link">{{ project.name }}</a></h4>
-        <p>{{ project.short_desc }}</p>
-      </div>
-
-      <div class="flex flex-row pb-4">
-        <div class="relative flex-grow">
-          <p>
-            <a :href="project.link" class="no-underline uppercase p-4 rounded-bl-lg" target="_blank" title="Open project repository">Project <i class="fas fa-link"></i></a>
-          </p>
+        <div class="p-4">
+          <h4><a :href="project.link">{{ project.name }}</a></h4>
+          <p>{{ project.short_desc }}</p>
         </div>
 
-        <div v-if="project.demo_link" class="text-right relative">
-          <p>
-            <a :href="project.demo_link" class="no-underline uppercase p-4 rounded-br-lg" title="Open project demo">Demo <i class="fas fa-laptop"></i></a>
-          </p>
+        <div class="flex flex-row pb-4">
+          <div class="relative flex-grow">
+            <p>
+              <a :href="project.link" class="no-underline uppercase p-4 rounded-bl-lg" target="_blank" title="Open project repository">Project <i class="fas fa-link"></i></a>
+            </p>
+          </div>
+
+          <div v-if="project.demo_link" class="text-right relative">
+            <p>
+              <a :href="project.demo_link" class="no-underline uppercase p-4 rounded-br-lg" title="Open project demo">Demo <i class="fas fa-laptop"></i></a>
+            </p>
+          </div>
         </div>
       </div>
 
