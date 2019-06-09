7fd5a5423112c8b67e86dc56a1a71cf07f3001c3
Elliot Derhay <e.j.derhay@gmail.com>
2019-06-09 15:44:07 -0400

    Update file indentation
    Blade file indentation was a little too much, especially if it's going
    to become a bit more complex.

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,32 +1,34 @@
 @extends('layouts.page')
 
 @section('content')
-    <div class="container mx-auto px-4 pt-6">
-        <section class="block">
+  <div class="container mx-auto px-4 pt-6">
+    <section class="block">
 
-            <div class="w-full pb-6 text-center">
-                <div class="px-2">
-                    <h1 class="content-title text-4xl pt-6 mt-4 md:pt-0 md:mt-0">
-                        Projects
-                    </h1>
-                    <p class="mb-4">
-                        Below are projects that I've either built myself or contributed directly to. Some will also have links to demos.
-                    </p>
-                </div>
-            </div>
+      <div class="w-full pb-6 text-center">
+        <div class="px-2">
+          <h1 class="content-title text-4xl pt-6 mt-4 md:pt-0 md:mt-0">
+            Projects
+          </h1>
+          <p class="mb-4">
+            Below are projects that I've either built myself or contributed directly to. Some will also have links to demos.
+          </p>
+        </div>
+      </div>
+
+    </section>
+
+    <section class="block w-full md:flex">
+
+      <div class="w-full pb-8 px-2 pt-6 mt-4 md:pt-0 md:mt-0">
+        <section id="main-projects-list-wrapper">
 
         </section>
+      </div>
 
-        <section class="block w-full md:flex">
-
-            <div class="w-full pb-8 px-2 pt-6 mt-4 md:pt-0 md:mt-0">
-                <section id="main-projects-list-wrapper"></section>
-            </div>
-
-        </section>
-    </div>
+    </section>
+  </div>
 @endsection
 
 @section('footer-extras')
-    <script src="{{ mix('/js/MainProjectsList.js') }}" charset="utf-8"></script>
+  <script src="{{ mix('/js/MainProjectsList.js') }}" charset="utf-8"></script>
 @endsection
