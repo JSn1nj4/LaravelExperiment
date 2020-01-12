9d4e64abae792ccd0e83e1b0bc9b50c66052ec98
Elliot Derhay <e.j.derhay@gmail.com>
2020-01-11 23:00:35 -0500

    Ensure 404 if projects page isn't enabled

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectsApiController;
 
 class ProjectsController extends Controller
@@ -11,6 +12,10 @@ class ProjectsController extends Controller
 
     public function __construct()
     {
+        if (!in_array(Route::currentRouteName(), config('app.enabled-pages'))) {
+            abort(404);
+        }
+
         $this->projectsApi = new ProjectsApiController;
     }
 
