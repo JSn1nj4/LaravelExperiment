f735f47cfa7f494a9f8e0ce25cb23c3c294dcecd
Elliot Derhay <e.j.derhay@gmail.com>
2020-04-21 21:58:54 -0400

    Minor changes to ResetPasswordController
    Changes:
    - Remove constructor
    - Use RouteServiceProvider::HOME to get redirect path

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Routing\Route;
 
 class ResetPasswordController extends Controller
 {
@@ -25,15 +27,5 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest');
-    }
+    protected $redirectTo = RouteServiceProvider::HOME;
 }
