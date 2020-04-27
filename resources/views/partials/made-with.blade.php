f911fee38782b379871c852736445177989b7b21
Elliot Derhay <e.j.derhay@gmail.com>
2020-04-27 14:49:25 -0400

    Move 'built with' to its own partial
    This is so this can easily be added easily on any part of the site,
    although I do think I'm going to remove it entirely from the homepage
    for now...

---
+++ resources/views/partials/made-with.blade.php
@@ -0,0 +1,56 @@
+<section>
+  <div class="container mx-auto px-4">
+    <div class="block text-center">
+      <h2 class="content-title text-2xl pt-6 mt-4 text-center">Built With</h2>
+
+      <div class="pb-6 text-center sm:inline-block">
+        <div class="px-2">
+
+          <p class="my-4">
+            <a href="https://laravel.com/" target="_blank">
+              <img class="inline" src="https://s3.amazonaws.com/elliotderhay-com/vectors/Laravel-l-slant-no-padding.svg" title="Laravel - The PHP Framework For Web Artisans" alt="Laravel logo" width="123" height="85">
+            </a>
+          </p>
+
+          <p class="mb-4 text-xl">
+            Laravel
+          </p>
+
+        </div>
+      </div>
+
+      <div class="pb-6 text-center sm:inline-block">
+        <div class="px-2">
+
+          <p class="my-4">
+            <a href="https://tailwindcss.com/" target="_blank">
+              <img class="inline" src="https://s3.amazonaws.com/elliotderhay-com/vectors/tailwindcss-no-shadow.svg" title="Tailwind CSS - A Utility-First CSS Framework for Rapid UI Development" alt="Tailwind CSS logo" width="85" height="85">
+            </a>
+          </p>
+
+          <p class="mb-4 text-xl">
+            Tailwind CSS
+          </p>
+
+        </div>
+      </div>
+
+      <div class="pb-6 text-center sm:inline-block">
+        <div class="px-2">
+
+          <p class="my-4">
+            <a href="https://vuejs.org/" target="_blank">
+              <img class="inline" src="https://s3.amazonaws.com/elliotderhay-com/vectors/vue-logo.svg" title="Tailwind CSS - A Utility-First CSS Framework for Rapid UI Development" alt="Tailwind CSS logo" width="98" height="85">
+            </a>
+          </p>
+
+          <p class="mb-4 text-xl">
+            Vue.js
+          </p>
+
+        </div>
+      </div>
+
+    </div>
+  </div>
+</section>
