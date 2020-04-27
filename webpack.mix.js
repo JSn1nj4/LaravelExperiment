dff84460794573491b05d2d89d049986c426431a
Elliot Derhay <e.j.derhay@gmail.com>
2020-04-26 22:19:23 -0400

    Finish replacing SCSS mixins with PostCSS mixins
    Changes:
    - Rebuild last mixin inline in mixins PostCSS plugin object
    - Remove old mixins file import from main CSS file
    - Rename calls to last mixin

--- webpack.mix.js
+++ webpack.mix.js
@@ -38,8 +38,6 @@ mix
     mixins({
       mixins: {
         transitions(mixin, settings) {
-          console.log(settings);
-
           settings = settings.indexOf('|') >= 0 ? settings.split('|') : [settings];
 
           let set = settings.reduce((set, val) => {
@@ -48,6 +46,21 @@ mix
 
           mixin.replaceWith({
             prop: 'transition',
+            value: set,
+          });
+        },
+        textGlow(mixin, number = 1, color = '#00C49A') {
+          let base = 2;
+          let size = 2;
+          let set = '';
+
+          for(let i = 0; i < number; i++) {
+            set = (set === '') ? `0 0 ${size}px ${color}` : `${set}, 0 0 ${size}px ${color}`;
+            size *= base;
+          }
+
+          mixin.replaceWith({
+            prop: 'text-shadow',
             value: set,
           });
         },
