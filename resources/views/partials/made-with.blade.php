ec5a8ca8bcdbcdd06e828967657c0a93097105c0
Elliot Derhay <e.j.derhay@gmail.com>
2020-04-27 15:13:24 -0400

    Make 'Made With' icon markup more modular
    This allows more easily editing the data with a shared structure while
    retaining the same markup structure across all items.
    
    Incidentally, this commit also fixes the alt and title text for the Vue
    icon.

--- resources/views/partials/made-with.blade.php
+++ resources/views/partials/made-with.blade.php
@@ -1,55 +1,53 @@
 <section>
   <div class="container mx-auto px-4">
     <div class="block text-center">
-      <h2 class="content-title text-2xl pt-6 mt-4 text-center">Built With</h2>
+      <h2 class="content-title text-2xl pt-6 mt-4 text-center">Made With</h2>
 
-      <div class="pb-6 text-center sm:inline-block">
-        <div class="px-2">
+      @foreach ([
+        [
+          'link' => 'https://laravel.com/',
+          'icon_src' => 'https://s3.amazonaws.com/elliotderhay-com/vectors/Laravel-l-slant-no-padding.svg',
+          'icon_title' => 'Laravel - The PHP Framework For Web Artisans',
+          'icon_alt' => 'Laravel logo',
+          'icon_width' => '123',
+          'icon_height' => '85',
+          'name' => 'Laravel',
+        ],
+        [
+          'link' => 'https://tailwindcss.com/',
+          'icon_src' => 'https://s3.amazonaws.com/elliotderhay-com/vectors/tailwindcss-no-shadow.svg',
+          'icon_title' => 'Tailwind CSS - A Utility-First CSS Framework for Rapidly Building Custom Designs',
+          'icon_alt' => 'Tailwind CSS logo',
+          'icon_width' => '85',
+          'icon_height' => '85',
+          'name' => 'Tailwind CSS',
+        ],
+        [
+          'link' => 'https://vuejs.org/',
+          'icon_src' => 'https://s3.amazonaws.com/elliotderhay-com/vectors/vue-logo.svg',
+          'icon_title' => 'Vue.js - The Progressive JavaScript Framework',
+          'icon_alt' => 'Vue.js logo',
+          'icon_width' => '98',
+          'icon_height' => '85',
+          'name' => 'Vue.js',
+        ],
+      ] as $item)
+        <div class="text-center sm:inline-block">
+          <div class="px-2">
 
-          <p class="my-4">
-            <a href="https://laravel.com/" target="_blank">
-              <img class="inline" src="https://s3.amazonaws.com/elliotderhay-com/vectors/Laravel-l-slant-no-padding.svg" title="Laravel - The PHP Framework For Web Artisans" alt="Laravel logo" width="123" height="85">
-            </a>
-          </p>
+            <p class="my-6">
+              <a href="{{ $item['link'] }}" target="_blank">
+                <img class="inline" src="{{ $item['icon_src'] }}" title="{{ $item['icon_title'] }}" alt="{{ $item['icon_alt'] }}" width="{{ $item['icon_width'] }}" height="{{ $item['icon_height'] }}">
+              </a>
+            </p>
 
-          <p class="mb-4 text-xl">
-            Laravel
-          </p>
+            <p class="mb-4 text-xl">
+              {{ $item['name'] }}
+            </p>
 
+          </div>
         </div>
-      </div>
-
-      <div class="pb-6 text-center sm:inline-block">
-        <div class="px-2">
-
-          <p class="my-4">
-            <a href="https://tailwindcss.com/" target="_blank">
-              <img class="inline" src="https://s3.amazonaws.com/elliotderhay-com/vectors/tailwindcss-no-shadow.svg" title="Tailwind CSS - A Utility-First CSS Framework for Rapid UI Development" alt="Tailwind CSS logo" width="85" height="85">
-            </a>
-          </p>
-
-          <p class="mb-4 text-xl">
-            Tailwind CSS
-          </p>
-
-        </div>
-      </div>
-
-      <div class="pb-6 text-center sm:inline-block">
-        <div class="px-2">
-
-          <p class="my-4">
-            <a href="https://vuejs.org/" target="_blank">
-              <img class="inline" src="https://s3.amazonaws.com/elliotderhay-com/vectors/vue-logo.svg" title="Tailwind CSS - A Utility-First CSS Framework for Rapid UI Development" alt="Tailwind CSS logo" width="98" height="85">
-            </a>
-          </p>
-
-          <p class="mb-4 text-xl">
-            Vue.js
-          </p>
-
-        </div>
-      </div>
+      @endforeach
 
     </div>
   </div>
