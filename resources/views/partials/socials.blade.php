5529c116c4fea4323d228d12522d810574006a60
Elliot Derhay <e.j.derhay@gmail.com>
2020-04-27 15:37:38 -0400

    Add Dev icon to footer

--- resources/views/partials/socials.blade.php
+++ resources/views/partials/socials.blade.php
@@ -10,4 +10,7 @@
   <a href="https://twitter.com/JSn1nj4"
     class="{{ $linkClasses }}"
     target="_blank"><i class="fab fa-twitter"></i></a>
+  <a href="https://dev.to/jsn1nj4"
+    class="{{ $linkClasses }}"
+    target="_blank"><i class="fab fa-dev"></i></a>
 </p>
