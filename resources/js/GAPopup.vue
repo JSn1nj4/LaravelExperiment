b4663bfd34e722634594847c02fab23aaee1d58f
Elliot Derhay <e.j.derhay@gmail.com>
2020-04-30 22:46:04 -0400

    Add spacer so popup doesn't block footer
    This spacer matches the height of the popup.
    
    Changes:
    - Put #dnt-box inside of a wrapper
    - Add #dnt-spacer element with data as height value
    - Set height value initially to auto
    - Add ref to #dnt-box to get height dynamically
    - Create method to set this new height when called
    - Run this method when the component mounts

--- resources/js/GAPopup.vue
+++ resources/js/GAPopup.vue
@@ -1,14 +1,17 @@
 <template>
-  <div id="dnt-box" class="fixed bottom-0 left-0 z-30 bg-gray-900 w-full p-8">
-    <div class="flex container mx-auto">
-      <div class="flex flex-grow h-full">
-        <p class="p-4 m-2">Please confirm whether you would like to allow tracking cookies on my site, in accordance with my <a href="/privacy-policy/">privacy policy</a>.</p>
-      </div>
-      <div class="flex w-64">
-        <button :class="`${btnClasses} bg-sea-green-600 text-gray-900`" @click="allowTracker(true)">Allow</button>
-        <button :class="`${btnClasses} bg-gray-800 text-sea-green-500`" @click="allowTracker(false)">Deny</button>
+  <div class="block w-full bg-gray-900">
+    <div id="dnt-box" class="fixed bottom-0 left-0 z-30 bg-gray-900 w-full p-8" ref="dntBox">
+      <div class="flex container mx-auto">
+        <div class="flex flex-grow h-full">
+          <p class="p-4 m-2">Please confirm whether you would like to allow tracking cookies on my site, in accordance with my <a href="/privacy-policy/">privacy policy</a>.</p>
+        </div>
+        <div class="flex w-64">
+          <button :class="`${btnClasses} bg-sea-green-600 text-gray-900`" @click="allowTracker(true)">Allow</button>
+          <button :class="`${btnClasses} bg-gray-800 text-sea-green-500`" @click="allowTracker(false)">Deny</button>
+        </div>
       </div>
     </div>
+    <div id="dnt-spacer" class="block w-full" :style="`height: ${boxHeight};`"></div>
   </div>
 </template>
 
@@ -16,11 +19,18 @@
 export default {
   data: () => ({
     'btnClasses': 'flex-1 p-4 m-2 font-bold',
+    'boxHeight': 'auto',
   }),
   methods: {
     allowTracker(allow) {
       this.$emit('allow_tracker', allow);
     },
+    setBoxHeight() {
+      this.boxHeight = `${this.$refs.dntBox.clientHeight}px`;
+    },
+  },
+  mounted() {
+    this.setBoxHeight();
   },
 }
 </script>
