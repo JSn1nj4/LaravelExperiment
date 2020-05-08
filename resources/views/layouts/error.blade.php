4777beda652d3b12bef22efda7f31ff33154e101
Elliot Derhay <e.j.derhay@gmail.com>
2020-05-08 15:38:29 -0400

    Set base size smaller, set code size large
    This was mainly done to reduce the size of the spaces around the
    code/message separator.

--- resources/views/layouts/error.blade.php
+++ resources/views/layouts/error.blade.php
@@ -3,10 +3,10 @@
 @section('content')
   <div class="flex flex-col mx-auto container max-h-screen h-screen px-4 py-6 justify-center">
     <div class="pb-4 w-full max-w-md mx-auto">
-      <h1 class="text-6xl">
-        <span class="align-middle">{{ $errorCode }}</span>
+      <h1 class="text-4xl">
+        <span class="align-middle text-6xl">{{ $errorCode }}</span>
         <span class="inline-block align-middle w-0 h-16 border-solid border-r-2 border-sea-green-500">&nbsp;</span>
-        <span class="text-4xl align-middle leading-none">{{ $errorTitle }}</span>
+        <span class="align-middle leading-none">{{ $errorTitle }}</span>
       </h1>
 
       <div class="pt-4 text-lg leading-normal font-thin">
