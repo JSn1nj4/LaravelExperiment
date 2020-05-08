f4c1529662f987f80399b8dfe77f0f7c731711b4
Elliot Derhay <e.j.derhay@gmail.com>
2020-05-08 16:16:35 -0400

    Thin error code weight, page backlight glow

--- resources/views/layouts/error.blade.php
+++ resources/views/layouts/error.blade.php
@@ -1,10 +1,10 @@
-@extends('layouts.splash', ['bodyClasses' => 'status-page'])
+@extends('layouts.splash', ['bodyClasses' => 'status-page backlight'])
 
 @section('content')
   <div class="flex flex-col mx-auto container max-h-screen h-screen px-4 py-6 justify-center">
     <div class="pb-4 w-full max-w-md mx-auto">
       <h1 class="text-4xl">
-        <span class="align-middle text-6xl">{{ $errorCode }}</span>
+        <span class="align-middle text-6xl font-thin" style="text-shadow:;">{{ $errorCode }}</span>
         <span class="inline-block align-middle w-0 h-16 border-solid border-r-2 border-sea-green-500">&nbsp;</span>
         <span class="align-middle leading-none">{{ $errorTitle }}</span>
       </h1>
