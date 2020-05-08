cd20e78aa866022bc8f35182419f4bd9f185399d
Elliot Derhay <e.j.derhay@gmail.com>
2020-05-08 16:48:25 -0400

    Update 403 page
    Changes:
    - Update Back to Homepage link
    - Add copyright and social icons

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -11,6 +11,14 @@
 
 @section('status-footer')
     <p>
-        <a href="{{ route('home') }}">Back to homepage</a>
+        <a class="text-white hover:text-sea-green-500" href="{{ route('home') }}"><i class="text-sea-green-500 fa fa-caret-square-left"></i> Back to homepage</a>
     </p>
+    <div class="pt-8 font-normal text-gray-500">
+      @include('partials.copyright')
+    </div>
+    <div class="pt-4">
+      @include('partials.socials', [
+        'classes' => 'text-2xl'
+      ])
+    </div>
 @endsection
