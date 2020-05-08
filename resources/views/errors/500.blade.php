bea4aef05d7c2f6a13186b9175ccc8858eac68a6
Elliot Derhay <e.j.derhay@gmail.com>
2020-05-08 16:46:27 -0400

    Update error 500 page
    Changes:
    - Slight text tweak
    - Ensure Twitter link isn't thin like other description text
    - Update Back to Homepage link

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -5,12 +5,12 @@
 
 @section('status-body')
     <p>
-        Something went wrong on the back-end. If this issue continues, please contact me via <a href="https://twitter.com/JSn1nj4">Twitter</a>.
+        Something went wrong on the back-end. If this issue persists, please contact me via <a class="font-normal" href="https://twitter.com/JSn1nj4">Twitter</a>.
     </p>
 @endsection
 
 @section('status-footer')
     <p>
-        <a href="{{ route('home') }}">Back to homepage</a>
+        <a class="text-white hover:text-sea-green-500" href="{{ route('home') }}"><i class="text-sea-green-500 fa fa-caret-square-left"></i> Back to homepage</a>
     </p>
 @endsection
