5c3f3c74528529863dfc25eef9780376e2508f69
Elliot Derhay <e.j.derhay@gmail.com>
2020-08-04 12:07:48 -0400

    Copy a method from parent provider

--- app/Providers/MarkdownServiceProvider.php
+++ app/Providers/MarkdownServiceProvider.php
@@ -2,10 +2,29 @@
 
 namespace App\Providers;
 
-use League\CommonMark\MarkdownConverterInterface;
 use GrahamCampbell\Markdown\MarkdownServiceProvider as GCMarkdownProvider;
 
 class MarkdownServiceProvider extends GCMarkdownProvider
 {
-    //
+    /**
+     * Override parent method for enabling Blade directive.
+     *
+     * @return string
+     */
+    protected function enableBladeDirective()
+    {
+        $app = $this->app;
+
+        $app['blade.compiler']->directive('markdown', function ($markdown) {
+            if ($markdown) {
+                return "<?php echo app('markdown')->convertToHtml((string) {$markdown}); ?>";
+            }
+
+            return '<?php ob_start(); ?>';
+        });
+
+        $app['blade.compiler']->directive('endmarkdown', function () {
+            return "<?php echo app('markdown.directive')->render(ob_get_clean()); ?>";
+        });
+    }
 }
