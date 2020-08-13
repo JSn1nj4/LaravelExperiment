a0caa9a95e683495a6a1fe306e298acf8a4fdcf5
Elliot Derhay <e.j.derhay@gmail.com>
2020-08-13 13:47:31 -0400

    Update privacy policy language

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -9,18 +9,12 @@
       <p class="font-bold mb-4">Last updated August 12, 2020</p>
 
       <h2 class="content-title text-3xl">What this policy covers</h2>
-      <p class="mb-4">This privacy policy applies to this Website (<a href="/">http://elliotderhay.com</a>) and how this Website does or does not collect and/or store information.</p>
+      <p class="mb-4">This privacy policy applies to this Website (<a href="/">http://elliotderhay.com</a>) and its collection of information. This policy does not cover how third-party websites linked to from this Website use personally-identifiable information. If this website links to any third-party websites, please see the privacy policies provided by those websites to learn how they store and/or process personally-identifiable information.</p>
 
-      <p class="mb-4">This policy does not cover how third-party websites linked to from this Website use personally-identifiable information.</p>
+      <h2 class="content-title text-3xl">Third Party Tools &amp; Services</h2>
+      <p class="mb-4">I, the Owner and sole developer of this Website, do not directly process any personal information. However, I do use the below listed tools and/or services. These are used for learning and Website improvement purposes only. They may also process personally-identifiable information in accordance with their own privacy policies.</p>
 
-      <h2 class="content-title text-3xl">Information Collected by Third Parties</h2>
-      <p class="mb-4">I do not directly collect or store any personal information. I do, however, use the following tools and/or services listed below. Each of these tools is for learning and Website improvement purposes only.</p>
-
-      <p class="mb-4">Additionally, even though this information may be stored according to each tool/service's privacy policy, I do not share this information with any other third parties myself.</p>
-
-      <p class="mb-4">Each item listed below has a link to a webpage about the tool/service itself and an additional link for its privacy policy. Please read and understand the terms of these privacy policies to determine if you are comfortable with how they collect information.</p>
-
-      <h3 class="content-title text-2xl">Third Party Tools &amp; Services</h3>
+      <p class="mb-4">Each item listed below has a link to a webpage describing the tool/service itself. An additional link is also provided for each item's privacy policy. Please read and understand the terms of these privacy policies to determine if you are comfortable with how they process information.</p>
 
       <ul class="mb-4 list-inside list-disc">
         <li><a href="https://marketingplatform.google.com/about/analytics/" target="_blank">Google Analytics</a>   (<a href="https://marketingplatform.google.com/about/analytics/terms/us/" target="_blank">Privacy Policy</a>)</li>
