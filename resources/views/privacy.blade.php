f1265914e5da8e0091092aa515b6678dff08d766
Elliot Derhay <e.j.derhay@gmail.com>
2020-08-18 13:11:49 -0400

    Update privacy policy text

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -9,16 +9,17 @@
       <p class="font-bold mb-4">Last updated August 12, 2020</p>
 
       <h2 class="content-title text-3xl">What this policy covers</h2>
-      <p class="mb-4">This privacy policy applies to this Website (<a href="/">http://elliotderhay.com</a>) and its collection of information. This policy does not cover how third-party websites linked to from this Website use personally-identifiable information. If this website links to any third-party websites, please see the privacy policies provided by those websites to learn how they store and/or process personally-identifiable information.</p>
+      <p class="mb-4">This privacy policy applies to this Website (<a href="/">http://elliotderhay.com</a>) and its collection of information. This policy does not cover how third-party websites linked to from this Website use personally-identifiable information. If this Website contains linkts to any third-party websites, please see the privacy policies provided by those websites to learn how they store and/or process personally-identifiable information.</p>
 
       <h2 class="content-title text-3xl">Third Party Tools &amp; Services</h2>
-      <p class="mb-4">I, the Owner and sole developer of this Website, do not directly process any personal information. However, I do use the below listed tools and/or services. These are used for learning and Website improvement purposes only. They may also process personally-identifiable information in accordance with their own privacy policies.</p>
-
-      <p class="mb-4">Each item listed below has a link to a webpage describing the tool/service itself. An additional link is also provided for each item's privacy policy. Please read and understand the terms of these privacy policies to determine if you are comfortable with how they process information.</p>
+      <p class="mb-4">This Website does not directly process any personal information, but it does use the below listed tools and/or services. They're used only for learning and website improvement purposes.</p>
 
       <ul class="mb-4 list-inside list-disc">
-        <li><a href="https://marketingplatform.google.com/about/analytics/" target="_blank">Google Analytics</a>   (<a href="https://marketingplatform.google.com/about/analytics/terms/us/" target="_blank">Privacy Policy</a>)</li>
+        <li><a href="https://marketingplatform.google.com/about/analytics/" target="_blank">Google Analytics</a> (<a href="https://marketingplatform.google.com/about/analytics/terms/us/" target="_blank">Privacy Policy</a>)</li>
       </ul>
+
+      <h2 class="content-title text-3xl">Cookies</h2>
+      <p class="mb-4">Currently, the only way this Website uses cookies directly is to allow or disallow Website visitor tracking via the third party services listed above. These services may set their own cookies as part of how they function. Please see their respective privacy policies to determine whether you're comfortable with how they use cookies.</p>
     </div>
   </section>
 
