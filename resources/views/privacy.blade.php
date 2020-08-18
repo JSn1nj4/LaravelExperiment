9276d054852a5b12e3f45718428c16915ca5eeb9
Elliot Derhay <e.j.derhay@gmail.com>
2020-08-18 13:12:50 -0400

    Update 'Last Updated' date on privacy policy page

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -6,7 +6,7 @@
     <div class="container mx-auto px-4 pt6">
       <h1 class="content-title text-4xl">Privacy Policy</h1>
 
-      <p class="font-bold mb-4">Last updated August 12, 2020</p>
+      <p class="font-bold mb-4">Last updated August 18, 2020</p>
 
       <h2 class="content-title text-3xl">What this policy covers</h2>
       <p class="mb-4">This privacy policy applies to this Website (<a href="/">http://elliotderhay.com</a>) and its collection of information. This policy does not cover how third-party websites linked to from this Website use personally-identifiable information. If this Website contains linkts to any third-party websites, please see the privacy policies provided by those websites to learn how they store and/or process personally-identifiable information.</p>
