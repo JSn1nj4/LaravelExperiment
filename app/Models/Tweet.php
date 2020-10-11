668c672a209cabbcbc6b85c02db0887523b47f69
Elliot Derhay <e.j.derhay@gmail.com>
2020-10-11 00:00:42 -0400

    Use TwitterClient in old Tweet "model" class
    This includes stripping out much of the code that was moved over to the TwitterClient class.

--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -2,153 +2,18 @@
 
 namespace App\Models;
 
+use App\Http\Clients\TwitterClient;
 use Illuminate\Database\Eloquent\Model;
 
 class Tweet extends Model
 {
-    /**
-     * The base Twitter API URL
-     *
-     * @property string         $api_url
-     * @access private
-     */
-    private $api_url = 'https://api.twitter.com';
+    private ?TwitterClient $twitter = null;
 
-    /**
-     * The token used for retrieving tweet information from the Twitter API
-     *
-     * @property string         $token
-     * @access private
-     */
-    private $token;
-
-    /**
-     * The API key used for generating the token
-     *
-     * @property string         $key
-     * @access private
-     */
-    private $key;
-
-    /**
-     * The API secret key part used for generating the token
-     *
-     * @property string         $secret
-     * @access private
-     */
-    private $secret;
-
-    /**
-     * Create a new instance of the Tweet model
-     *
-     * @method                  __construct
-     * @access public
-     *
-     * @param array             $attributes
-     *
-     * @return void
-     *
-     * This is necessary to initialize some properties that can't otherwise be
-     * initialized. Initializing properties outside of a constrctor requires
-     * that the initial values be static.
-     */
     public function __construct(array $attributes = [])
     {
         parent::__construct($attributes);
 
-        $this->token = config('services.twitter.token', false);
-        $this->key = config('services.twitter.key', false);
-        $this->secret = config('services.twitter.secret', false);
-    }
-
-    /**
-     * Generate a new Twitter API token if one doesn't exist
-     *
-     * @method                  createToken
-     * @access public
-     *
-     * @return void
-     *
-     * The 'if' in this case has to do with whether Twitter already has a token
-     * for use. If the token hasn't been generated previously or if the previous
-     * token was revoked, then this function will be used to ask Twitter for a
-     * new one.
-     *
-     * This method will also only be called if the token doesn't already exist
-     * in the environment.
-     */
-    public function createToken()
-    {
-        if($this->token) {
-            return true;
-        }
-
-        if(!$this->key || !$this->secret) {
-            abort(500);
-        }
-
-        $post_url = "$this->api_url/oauth2/token";
-
-        $twitter_str = base64_encode(urlencode($this->key) . ':' . urlencode($this->secret));
-
-        $ch = curl_init();
-
-        curl_setopt_array($ch, [
-            CURLOPT_URL => $post_url,
-            CURLOPT_POST => 1,
-            CURLOPT_HTTPHEADER => [
-                "Authorization: Basic $twitter_str",
-                "Content-Type: application/x-www-form-urlencoded;charset=UTF-8"
-            ],
-            CURLOPT_POSTFIELDS => "grant_type=client_credentials",
-            CURLOPT_RETURNTRANSFER => true
-        ]);
-
-        $result = json_decode(curl_exec($ch));
-        curl_close($ch);
-
-        if(isset($result->errors)) {
-            abort(500);
-        }
-
-        $this->token = $result->access_token;
-    }
-
-    /**
-     * Retrieve raw tweets via Twitter's GET API
-     *
-     * @method                  getRawTweets
-     * @access public
-     *
-     * @param string            $curl_url
-     *
-     * @return string
-     */
-    public function getRawTweets(string $curl_url)
-    {
-        if(!$this->token) {
-            $this->createToken();
-        }
-
-        $ch = curl_init();
-
-        curl_setopt_array($ch, [
-            CURLOPT_URL => $curl_url,
-            CURLOPT_HTTPHEADER => [
-                "Authorization: Bearer $this->token"
-            ],
-            CURLOPT_RETURNTRANSFER => true
-        ]);
-
-        $tweets = curl_exec($ch);
-
-        if(isset(json_decode($tweets)->errors)) {
-            abort(500);
-        }
-
-        curl_close($ch);
-
-        return $tweets;
+        $this->twitter = new TwitterClient;
     }
 
     /**
@@ -216,8 +81,9 @@ class Tweet extends Model
         $retweets = isset($options['retweets']) && is_bool($options['retweets']) ?
             $options['retweets'] : true;
 
-        $url = "$this->api_url/1.1/statuses/user_timeline.json?count=$count&screen_name=$screen_name&include_rts=$retweets";
-        return $this->formatTweetData($this->getRawTweets($url));
+        return $this->formatTweetData(
+            $this->twitter->getTweets($screen_name, null, $retweets, $count)
+        );
     }
 
     /**
@@ -236,7 +102,6 @@ class Tweet extends Model
      */
     public function getTweet($tweet_id)
     {
-        $url = "$this->api_url/1.1/statuses/show.json?id=$tweet_id";
-        return $this->formatTweetData("[" . $this->getRawTweets($url) . "]");
+        return $this->formatTweetData("[" . $this->twitter->getSingleTweet($tweet_id) . "]");
     }
 }
