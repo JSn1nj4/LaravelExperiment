2a1410a6e9873eb90abab4b84171a5e5e511baad
Elliot Derhay <e.j.derhay@gmail.com>
2020-10-13 14:07:03 -0400

    Set up initial data structure for Tweets
    This includes ensuring tweet entities are automatically encoded on saving and decoded on retrieval.

--- database/migrations/2020_10_12_160714_create_tweets_table.php
+++ database/migrations/2020_10_12_160714_create_tweets_table.php
@@ -15,6 +15,12 @@ class CreateTweetsTable extends Migration
     {
         Schema::create('tweets', function (Blueprint $table) {
             $table->id();
+            $table->integer('tweet_id');
+            $table->integer('user_id');
+            $table->text('body');
+            $table->timestamp('date');
+            $table->integer('sub_tweet_id')->nullable();
+            $table->json('entities');
             $table->timestamps();
         });
     }
