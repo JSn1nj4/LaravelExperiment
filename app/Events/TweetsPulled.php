144d203103bd5805e21e67e0660fe6a400b44d68
Elliot Derhay <e.j.derhay@gmail.com>
2020-10-18 22:50:07 -0400

    Remove a ton of default event fluff

--- app/Events/TweetsPulled.php
+++ app/Events/TweetsPulled.php
@@ -2,17 +2,11 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Queue\SerializesModels;
 
 class TweetsPulled
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable;
 
     /**
      * Create a new event instance.
@@ -22,15 +16,5 @@ class TweetsPulled
     public function __construct()
     {
         //
-    }
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
-    public function broadcastOn()
-    {
-        return new PrivateChannel('channel-name');
     }
 }
