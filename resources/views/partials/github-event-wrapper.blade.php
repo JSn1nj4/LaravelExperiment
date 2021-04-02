ae26fcb57cc372b5428f4e5e8cc5cad68072a9c6
Elliot Derhay <e.j.derhay@gmail.com>
2021-04-02 19:16:16 -0400

    Replace card partial

--- resources/views/partials/github-event-wrapper.blade.php
+++ resources/views/partials/github-event-wrapper.blade.php
@@ -1,16 +1,12 @@
 @php
-  use App\Helpers\GlobalHelpers;
-  $type = GlobalHelpers::stringToKebabCase($event->type);
+	use App\Helpers\GlobalHelpers;
+	$type = GlobalHelpers::stringToKebabCase($event->type);
 @endphp
 
 <div class="github-event-item">
-  @component('partials.card', [
-    'size' => 'sm',
-    'type' => 'transparent',
-    'margin' => 'my-4'
-  ])
-    @include("partials.github-event-types.$type", [
-      'event' => $event
-    ])
-  @endcomponent
+	<x-card size="sm" type="transparent" margin="my-4">
+		@include("partials.github-event-types.$type", [
+			'event' => $event
+		])
+	</x-card>
 </div>
