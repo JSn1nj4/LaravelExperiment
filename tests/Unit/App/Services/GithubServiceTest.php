9ad82e2859694df3b0e0256daf15281767be46e9
Elliot Derhay <JSn1nj4@users.noreply.github.com>
2021-06-08 18:31:27 -0400

    Rename GithubService > GithubServiceTest

--- tests/Unit/App/Services/GithubServiceTest.php
+++ tests/Unit/App/Services/GithubServiceTest.php
@@ -2,13 +2,6 @@
 
 use App\Services\GithubService;
 
-use function Pest\Laravel\mock;
-
-beforeEach(function() {
-	$this->config = mock('Illuminate\Config\Repository');
-	// need to add some fake return values for config options that are looked for
-});
-
 test('creates an instance of App\Services\GithubService', function () {
 	$githubService = new GithubService;
 
