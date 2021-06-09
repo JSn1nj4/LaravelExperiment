df013ffe284c190da9394db90422c9222d282ed2
Elliot Derhay <JSn1nj4@users.noreply.github.com>
2021-06-09 13:24:18 -0400

    Run Browser tests with Pest

--- tests/Browser/HomePageTest.php
+++ tests/Browser/HomePageTest.php
@@ -1,56 +1,46 @@
 <?php
 
-namespace Tests\Browser;
-
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\Browser\Pages\HomePage;
-use Tests\DuskTestCase;
 
-class HomePageTest extends DuskTestCase
-{
-	public function testVisitHomePage(): void
-	{
-		$this->browse(function (Browser $browser) {
-			$homepage = new HomePage;
+it('can visit the homepage', function (): void {
+	$this->browse(function (Browser $browser) {
+		$homepage = new HomePage;
 
-			$browser->visit($homepage)
-				->assertVisible('@main')
-				->assertVisible('@banner')
-				->assertSee('A personal profile')
-				->assertSee('About Me')
-				->assertVisible('@twitter')
-				->assertVisible('@github');
-		});
-	}
+		$browser->visit($homepage)
+			->assertVisible('@main')
+			->assertVisible('@banner')
+			->assertSee('A personal profile')
+			->assertSee('About Me')
+			->assertVisible('@twitter')
+			->assertVisible('@github');
+	});
+});
 
-	public function testLeaveHomePageAndNavigateBackToHomePage(): void
-	{
-		$this->browse(function (Browser $browser) {
-			$homepage = new HomePage;
+it('can leave the homepage and navigate back to the homepage', function (): void {
+	$this->browse(function (Browser $browser) {
+		$homepage = new HomePage;
 
-			$browser->visit($homepage)
-				->within($homepage->components->header, function (Browser $browser) use ($homepage) {
-					$browser->clickLink('Projects')
-						->assertPathIs(route('projects', absolute: false))
-						->clickLink('Home')
-						->assertPathIs($homepage->url());
-				});
-		});
-	}
+		$browser->visit($homepage)
+			->within($homepage->components->header, function (Browser $browser) use ($homepage) {
+				$browser->clickLink('Projects')
+					->assertPathIs(route('projects', absolute: false))
+					->clickLink('Home')
+					->assertPathIs($homepage->url());
+			});
+	});
+});
 
-	public function testVisitPrivacyPolicyPage(): void
-	{
-		$this->browse(function (Browser $browser) {
-			$homepage = new HomePage;
+it('can visit the privacy policy page', function (): void {
+	$this->browse(function (Browser $browser) {
+		$homepage = new HomePage;
 
-			$browser->visit($homepage)
-				->within($homepage->components->footer, function (Browser $browser) {
-					$browser->assertSee('Copyright')
-						->assertVisible('@privacy_link')
-						->clickLink('Privacy Policy')
-						->assertPathIs(route('privacy', absolute: false));
-				});
-		});
-	}
-}
+		$browser->visit($homepage)
+			->within($homepage->components->footer, function (Browser $browser) {
+				$browser->assertSee('Copyright')
+					->assertVisible('@privacy_link')
+					->clickLink('Privacy Policy')
+					->assertPathIs(route('privacy', absolute: false));
+			});
+	});
+});
