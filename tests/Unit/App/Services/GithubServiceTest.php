ff58a45ba59ba7809b2664867ba892cfe63fbb27
Elliot Derhay <JSn1nj4@users.noreply.github.com>
2021-06-09 13:26:22 -0400

    Use `it` instead of `test`

--- tests/Unit/App/Services/GithubServiceTest.php
+++ tests/Unit/App/Services/GithubServiceTest.php
@@ -2,7 +2,7 @@
 
 use App\Services\GithubService;
 
-test('creates an instance of App\Services\GithubService', function () {
+it('creates an instance of App\Services\GithubService', function () {
 	$githubService = new GithubService;
 
 	expect($githubService)->toBeInstanceOf(GithubService::class);
