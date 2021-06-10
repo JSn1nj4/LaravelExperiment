0187aea62d71e8783f183816215be4f86ce8f97e
Elliot Derhay <JSn1nj4@users.noreply.github.com>
2021-06-10 14:05:33 -0400

    Add a new test and start another

--- tests/Unit/App/Services/GithubServiceTest.php
+++ tests/Unit/App/Services/GithubServiceTest.php
@@ -1,9 +1,49 @@
 <?php
 
 use App\Services\GithubService;
+use Illuminate\Support\Facades\Http;
 
-it('creates an instance of App\Services\GithubService', function () {
-	$githubService = new GithubService;
+use function Pest\Laravel\mock;
 
-	expect($githubService)->toBeInstanceOf(GithubService::class);
+beforeEach(function (): void {
+	$this->api_base = 'https://api.github.com';
+	$this->githubService = new GithubService;
+});
+
+it('creates an instance of App\Services\GithubService', function (): void {
+	expect($this->githubService)->toBeInstanceOf(GithubService::class);
+});
+
+it('constructs the correct api url', function (): void {
+	$api_endpoint = 'users/jsn1nj4/events/public';
+
+	expect($this->githubService->getUrl($api_endpoint))
+		->toBeString()
+		->toEqual("{$this->api_base}/{$api_endpoint}");
+});
+
+// it('can use the github api with an active token', function (): void {
+
+// });
+
+// it('cannot use the github api without an active token', function (): void {
+
+// });
+
+it('can fetch user events from the correct endpoint', function (): void {
+	$response = (object) [
+		'body' => [
+			1, 2, 3,
+		],
+		'status' => 200,
+		'headers' => [
+			'test1', 'test2', 'test3',
+		],
+	];
+
+	Http::fake([
+		"{$this->api_base}/users/jsn1nj4/events/public" => Http::response($response->body, $response->status, $response->headers),
+	]);
+
+	$this->assertTrue(true);
 });
