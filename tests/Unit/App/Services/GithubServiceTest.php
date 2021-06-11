304e49204ae51c63c52706f9ae8c66c8ed247747
Elliot Derhay <JSn1nj4@users.noreply.github.com>
2021-06-11 13:20:25 -0400

    Update behaviors expected to test

--- tests/Unit/App/Services/GithubServiceTest.php
+++ tests/Unit/App/Services/GithubServiceTest.php
@@ -14,6 +14,16 @@ it('creates an instance of App\Services\GithubService', function (): void {
 	expect($this->githubService)->toBeInstanceOf(GithubService::class);
 });
 
+it('throws an exception if no token is found', function (): void {
+	// token value currently set to `false` if no token is found in environment
+});
+
+it('throws an exception if no email recipient is defined', function (): void {
+	// information should be found in config
+
+	// recipients should be defined in $alertRecipients
+});
+
 it('constructs the correct api url', function (): void {
 	$api_endpoint = 'users/jsn1nj4/events/public';
 
@@ -22,15 +32,20 @@ it('constructs the correct api url', function (): void {
 		->toEqual("{$this->api_base}/{$api_endpoint}");
 });
 
-// it('can use the github api with an active token', function (): void {
+it('uses a correctly-formatted event api request', function (): void {
+	// need to match the request it sends with the one defined here
+});
 
-// });
+it('rejects an incorrectly-formatted event api response', function(): void {
+	// ...
+});
 
-// it('cannot use the github api without an active token', function (): void {
+it('handles api errors', function (): void {
+	// determine what happens currently when an error is received.
+	// sending a request with a bad token should be enough
+});
 
-// });
-
-it('can fetch user events from the correct endpoint', function (): void {
+it('processes response data received from the github events api', function (): void {
 	$response = (object) [
 		'body' => [
 			1, 2, 3,
@@ -46,4 +61,14 @@ it('can fetch user events from the correct endpoint', function (): void {
 	]);
 
 	$this->assertTrue(true);
+});
+
+it('filters out unsupported types of github events', function (): void {
+	// for this to work, need to spy on service object to see supported event types
+});
+
+it('emails a list of new event types that should be supported', function (): void {
+	// requires email connection be configured
+
+	// look into how to fake email being sent
 });
