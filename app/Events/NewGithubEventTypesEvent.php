89bc649cc79082786a35c2f339b92a034be8e5ac
Elliot Derhay <JSn1nj4@users.noreply.github.com>
2021-06-18 17:26:09 -0400

    Use correct Collection class

--- app/Events/NewGithubEventTypesEvent.php
+++ app/Events/NewGithubEventTypesEvent.php
@@ -2,7 +2,7 @@
 
 namespace App\Events;
 
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Collection;
 use Illuminate\Foundation\Events\Dispatchable;
 
 class NewGithubEventTypesEvent
